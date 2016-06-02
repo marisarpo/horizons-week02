56426895bdc23f5ad7ebc35cc9d24deec71bcbb4
Ethan Lee <ethanlee@ethansites.com>
2016-06-02 14:58:17 -0400

    Fixed tests

--- day3/1_handling_events/event_handlers_tests.js
+++ day3/1_handling_events/event_handlers_tests.js
@@ -1,13 +1,21 @@
 describe("attachAlerts(className)", function() {
-  console.log(spyOnEvents)
-  var spy = [spyOnEvent('#red', 'click'), spyOnEvent('#blue', 'click'), spyOnEvent('#nope', 'click')];
+  var red = $('#red');
+  var blue = $('#blue');
+  var nope = $('#nope');
+  var spy = [spyOnEvent(red, 'click'), 
+             spyOnEvent(blue, 'click'), 
+             spyOnEvent(nope, 'click')];
+  console.log(spy)
   it("should alert when you cut the red wire", function() {
+    red.click();
     expect(spy[0]).toHaveBeenTriggered();
   });
   it("should alert when you cut the blue wire", function() {
+    blue.click();
     expect(spy[1]).toHaveBeenTriggered();
   });
   it("should alert when you (try to) run", function() {
+    nope.click();
     expect(spy[2]).toHaveBeenTriggered();
   });
 });

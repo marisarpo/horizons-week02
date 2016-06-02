a34d2ee3917d71b1f752d001c952d930f2874834
Ethan Lee <ethanlee@ethansites.com>
2016-06-02 14:48:17 -0400

    Added initial tests

--- day3/1_handling_events/event_handlers_tests.js
+++ day3/1_handling_events/event_handlers_tests.js
@@ -1,3 +1,13 @@
 describe("attachAlerts(className)", function() {
-  
+  console.log(spyOnEvents)
+  var spy = [spyOnEvent('#red', 'click'), spyOnEvent('#blue', 'click'), spyOnEvent('#nope', 'click')];
+  it("should alert when you cut the red wire", function() {
+    expect(spy[0]).toHaveBeenTriggered();
+  });
+  it("should alert when you cut the blue wire", function() {
+    expect(spy[1]).toHaveBeenTriggered();
+  });
+  it("should alert when you (try to) run", function() {
+    expect(spy[2]).toHaveBeenTriggered();
+  });
 });

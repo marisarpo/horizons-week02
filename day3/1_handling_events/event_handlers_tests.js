44a59d2ad8083925ff5874d86c43ac8ac8a32c67
Ethan Lee <ethanlee@ethansites.com>
2016-06-05 12:13:22 -0400

    Copying changes to blank template

--- day3/1_handling_events/event_handlers_tests.js
+++ day3/1_handling_events/event_handlers_tests.js
@@ -1,24 +1,30 @@
-describe("attachAlerts(className)", function() {
-  var red = $('#red');
-  var blue = $('#blue');
-  var nope = $('#nope');
-  var spy = [spyOnEvent(red, 'click'), 
-             spyOnEvent(blue, 'click'), 
-             spyOnEvent(nope, 'click')];
-  //spyOn(window, 'alert');
-  it("should alert when you cut the red wire", function() {
-    red.click();
-    expect(spy[0]).toHaveBeenTriggered();
-    expect(window.alert).toHaveBeenCalledWith('Bad choice!');
-  });
-  it("should alert when you cut the blue wire", function() {
-    blue.click();
-    expect(spy[1]).toHaveBeenTriggered();
-    expect(window.alert).toHaveBeenCalledWith('Bad choice!');
-  });
-  it("should alert when you (try to) run", function() {
-    nope.click();
-    expect(spy[2]).toHaveBeenTriggered();
-    expect(window.alert).toHaveBeenCalledWith('Bad choice!');
-  });
-});
+//describe("attachAlerts(className)", function() {
+  //var red = $('#red');
+  //var blue = $('#blue');
+  //var nope = $('#nope');
+  ////spyOn(window, 'alert');
+  //var spy = [spyOnEvent(red, 'click'),
+             //spyOnEvent(blue, 'click'),
+             //spyOnEvent(nope, 'click')];
+
+  //it("should alert when you cut the red wire", function() {
+    //red.click();
+    //expect(spy[0]).toHaveBeenTriggered();
+    //expect(window.alert).toHaveBeenCalledWith('Bad choice!');
+  //});
+  //it("should alert when you cut the blue wire", function() {
+    //blue.click();
+    //expect(spy[1]).toHaveBeenTriggered();
+    //expect(window.alert).toHaveBeenCalledWith('Bad choice!');
+  //});
+  //it("should alert when you (try to) run", function() {
+    //nope.click();
+    //expect(spy[2]).toHaveBeenTriggered();
+    //expect(window.alert).toHaveBeenCalledWith('Bad choice!');
+  //});
+//});
+
+//jasmine.getFixtures().fixturesPath = '../staging';
+//loadFixtures('event_handlers.html');
+//expect($('#red')).toHandle('click');
+

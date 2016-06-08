ca6385e875da8ff9b8ccd96116b3f437d9442a8d
Lane Rettig <lanerettig@gmail.com>
2016-06-08 15:31:57 -0400

    cleaning up some code. first write call is working (create card).

--- day4/horello-ajax/solution/js/1_data_model.js
+++ day4/horello-ajax/solution/js/1_data_model.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.horello = {};
+window.horello = window.horello || {};
 
 horello.Card = function(id, title, desc, listId) {
   this.id = id;
@@ -28,6 +28,24 @@ horello.Card.prototype = {
 
   setDescription: function(desc) {
     this.desc = desc;
+  },
+
+  render: function() {
+    // build wrappers
+    var wrapper = $('<div></div>');
+    var cardwrapper = $('<div class="card" data-list-id="'+this.listId+'" data-card-id="'+this.id+'"></div>');
+    var cardmore = $('<span class="card-more"></span>');
+    if (this.getDescription()) {
+      cardmore.append($('<span class="glyphicon glyphicon-align-left"></span>'));
+    }
+    var cardbody = $('<div class="card-body">'+this.title+'</div>');
+
+    wrapper.append(cardwrapper);
+    cardwrapper.append(cardmore);
+    cardwrapper.append(cardbody);
+    cardbody.append($("<p></p>")).text(this.title);
+
+    return wrapper.html();
   }
 };
 
@@ -51,9 +69,19 @@ horello.List.prototype = {
   },
 
   addCard: function(name, desc) {
-    var card = new horello.Card(name, desc, this.getId());
-    this.cards.push(card);
-    return card.getId();
+    // First create the data in the API.
+    Trello.post("/cards", {
+      name: name,
+      desc: desc,
+      idList: this.id
+    }, function (data) {
+      // Success! Now we have an ID and we can create it locally. Or,
+      // we can reload the data from the API.
+      console.log("Successfully created new card: " + JSON.stringify(data));
+      this.loadCards();
+    }.bind(this), function (err) {
+      console.error("Error creating new card: " + JSON.stringify(err));
+    });
   },
 
   getCard: function(cardId) {
@@ -74,7 +102,65 @@ horello.List.prototype = {
     var ind = this.cards.indexOf(c);
     this.cards.splice(ind, 1);
     return c;
-  }
+  },
+
+  render: function() {
+    // Build wrappers
+    var wrapper = $('<div></div>');
+
+    var listContainer = $('<div class="list-container"></div>');
+    var listWrapper = $('<div class="list" id="'+this.id+'"></div>');
+    var listHeader = $('<div class="list-header"></div>');
+    var listBody = $('<div class="list-cards"></div>');
+    var listFooter = $('<div class="list-footer"></div>');
+
+    wrapper.append(listContainer);
+    listContainer.append(listWrapper);
+    listWrapper.append(listHeader);
+    listWrapper.append(listBody);
+    listWrapper.append(listFooter);
+    listHeader.append($('<span class="list-title"></span>').text(this.name));
+    listFooter.append($('<button class="add-card" addCardId="'+this.id+'">Add a card...</button>'));
+    listFooter.append($('\
+      <div class="collapse" id="addCardForm'+this.id+'">\
+      <div class="well add-card-form">\
+      <input type="text" class="form-control" placeholder="Card title" id="addCardTitle'+this.id+'">\
+      <button type="button" class="btn btn-default" id="addCardBtn'+this.id+'">\
+      Save\
+      </button>\
+      <button type="button" class="btn btn-default">\
+      <span class="glyphicon glyphicon-remove" id="addCardCancelBtn'+this.id+'"></span>\
+      </button>\
+      </div>\
+      </div>\
+    '));
+
+    // Build cards in the body
+    listBody.html(this.cards.reduce(function(prev, cur) {
+      return prev + cur.render();
+    }, ""));
+
+    return wrapper.html();
+  },
+
+  loadCards: function() {
+    Trello.get(
+      "/lists/" + this.id + "/cards",
+      function (data2) {
+        console.log("Successfully loaded cards for list " + this.id);
+        this.cards = data2.map(horello.Card.fromJSON);
+
+        // Add this list to the board if it's not already there.
+        // if (!board.getList(this.id))
+        //   board.lists.push(this);
+
+        // Re-render.
+        horello.mount(board);
+      }.bind(this), function (err) {
+        console.error("Error loading cards for list " + data.id + ": " + JSON.stringify(err));
+      }
+    );
+  },
 };
 
 horello.Board = function () {
@@ -102,12 +188,19 @@ horello.Board.prototype = {
     var ind = this.lists.indexOf(c);
     this.lists.splice(ind, 1);
     return c;
+  },
+  
+  render: function() {
+    var wrapper = $('<div id="board" class="board"></div>');
+    wrapper.html(this.lists.reduce(function(prev, cur) {
+      return prev + cur.render();
+    }, ""));
+    return wrapper;
   }
 };
 
 horello.Board.download = function(id) {
-  Trello.rest(
-    "GET",
+  Trello.get(
     "/boards/" + id + "/lists",
     function (data2) {
       console.log("Successfully loaded lists for board " + id);
@@ -123,21 +216,12 @@ horello.Board.download = function(id) {
 
 horello.List.fromJSON = function(data) {
   var list = new horello.List(data.id, data.name);
-  Trello.rest(
-    "GET",
-    "/lists/" + data.id + "/cards",
-    function (data2) {
-      console.log("Successfully loaded cards for list " + data.id);
-      list.cards = data2.map(horello.Card.fromJSON);
-      board.lists.push(list);
-      horello.mount(board);
-    }, function (err) {
-      console.error("Error loading cards for list " + data.id + ": " + JSON.stringify(err));
-    }
-  );
+  board.lists.push(list);
+  list.loadCards();
 };
 
 horello.Card.fromJSON = function(data) {
   var card = new horello.Card(data.id, data.name, data.desc, data.idList);
   return card;
 };
+

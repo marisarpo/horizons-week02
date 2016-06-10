cd741a037321aa4481cb601464d60af9555e69d9
Adam He <shuaihe2018@u.northwestern.edu>
2016-06-10 16:02:36 -0400

    day 4 - 3

--- day4/3_momentum/js/momentum-weather.js
+++ day4/3_momentum/js/momentum-weather.js
@@ -5,17 +5,19 @@ window.momentum = window.momentum || {};
 // Weather
 
 momentum.WeatherCtrl = function() {
-  this.apiKey = "YOUR APP ID HERE";
+  this.apiKey = "aede7657bab41a5187c9e8fc70876b80";
   this.apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&APPID=" + this.apiKey;
 };
 
 momentum.WeatherCtrl.prototype = {
-	// `fetchWeather(cb<Function>)` method
-	// This function should fetch the current weather in Philly by performing an AJAX call. It should pass the given cb (callback) function to the success property of the call.
-	// 
-	// hint. look into $.ajax here: http://api.jquery.com/jquery.ajax/
-	// hint. read through the documentation for the OpenWeatherAPI.
+  // `fetchWeather(cb<Function>)` method
+  // This function should fetch the current weather in Philly by performing an AJAX call. It should pass the given cb (callback) function to the success property of the call.
+  //
+  // hint. look into $.ajax here: http://api.jquery.com/jquery.ajax/
+  // hint. read through the documentation for the OpenWeatherAPI.
   fetchWeather: function(cb) {
-		// YOUR CODE HERE
+    $.ajax(this.apiUrl, {
+      success: cb,
+    })
   }
 };

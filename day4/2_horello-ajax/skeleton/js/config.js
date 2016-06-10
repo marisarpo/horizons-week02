726d7cb5d394c6e01530897829b05a0c4474372e
kitchena <kitchena@wharton.upenn.edu>
2016-06-10 09:16:22 -0400

    horello

--- day4/2_horello-ajax/skeleton/js/config.js
+++ day4/2_horello-ajax/skeleton/js/config.js
@@ -2,6 +2,6 @@
 
 window.horello = window.horello || {};
 
-horello.apiKey = "YOUR API KEY HERE";
-horello.apiToken = "YOUR API TOKEN HERE";
+horello.apiKey = "15d4cc8a314d5d76847eda9cbaa2b878";
+horello.apiToken = "9ca61e0018bca1f70af01082773b2a8e40d88e43c87d32ad6bddd41ff8c0aff5";
 horello.apiUrl = "https://api.trello.com/1";

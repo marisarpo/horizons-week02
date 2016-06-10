948dd33e62526d6d40397e41ca3a4a71b8aa848a
Virginia Van Keuren <virginiavankeuren2017@u.northwestern.edu>
2016-06-10 10:19:26 -0400

    made changes on Thursday

--- day3/5_making_horello_dynamic/skeleton/js/2_render.js
+++ day3/5_making_horello_dynamic/skeleton/js/2_render.js
@@ -24,6 +24,37 @@ horello.Card.prototype.render = function() {
 // representing the internal object.
 horello.List.prototype.render = function() {
   // YOUR CODE HERE
+  var wrapper = $('<div></div>');
+  var listcontainering =$('<div class="list-container"></div>');
+  var listwrapper = $('<div class="list"></div>');
+  var listheader = $('<div class="list-header"></div>');
+  var listtitler = $('<span class="list-title"></span>').text(this.name);
+  var listcarder = $('<div class="list-cards"></div>');
+  var listfooter = $('<div class="list-footer"></div>');
+
+  wrapper.append(listcontainering);
+  listcontainering.append(listwrapper);
+  listwrapper.append(listheader);
+  listwrapper.append(listcarder);
+  listwrapper.append(listfooter);
+  listheader.append(listtitler);
+  listfooter.append($("<button class="add-card" addCardId="+this.id+">Add a card...</button>"));
+  listfooter.append($('\
+      <div class="collapse" id="addCardForm'+this.id+'">\
+      <div class="well add-card-form">\
+      <input type="text" class="form-control" placeholder="Card title" id="addCardTitle'+this.id+'">\
+      <button type="button" class="btn btn-default" id="addCardBtn'+this.id+'">\
+      Save\
+      </button>\
+      <button type="button" class="btn btn-default">\
+      <span class="glyphicon glyphicon-remove" id="addCardCancelBtn'+this.id+'"></span>\
+      </button>\
+      </div>\
+      </div>\
+    '));
+
+
+  return wrapper.html();
 }
 
 // Phase 3. Board

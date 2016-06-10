ba3a5bf0be1ae2aa2c61dcf90ffa4ef136cf1f94
David Pu <push0216@gmail.com>
2016-06-10 15:12:32 -0400

    ajax done

--- day4/2_horello-ajax/skeleton/js/config.js
+++ day4/2_horello-ajax/skeleton/js/config.js
@@ -5,3 +5,4 @@ window.horello = window.horello || {};
 horello.apiKey = "13b4ced4301f11306c4c8c0262c52fc1";
 horello.apiToken = "4740039c21be858c027780b04cab81de2d6d7cd6bc25946e76d194f5044c1f34";
 horello.apiUrl = "https://api.trello.com/1";
+horello.boardId = "5759ea94e94a05f94ecb8e33";

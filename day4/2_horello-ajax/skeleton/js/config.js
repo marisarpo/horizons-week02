cd09d25e1a5027a51323f99a6b9bc32b714b768f
tconcannon <taycon@seas.upenn.edu>
2016-06-10 14:31:28 -0400

    k;l;

--- day4/2_horello-ajax/skeleton/js/config.js
+++ day4/2_horello-ajax/skeleton/js/config.js
@@ -2,6 +2,6 @@
 
 window.horello = window.horello || {};
 
-horello.apiKey = "YOUR API KEY HERE";
-horello.apiToken = "YOUR API TOKEN HERE";
+horello.apiKey = "4426aaac3e3ddf941ff93930255038d8";
+horello.apiToken = "2a9829c4a7de4076e6c22b25237f356586253597b6760047cdddc0225923eaae";
 horello.apiUrl = "https://api.trello.com/1";

eb25d698e48aacf43275b9676773eab54364db53
Julian Mullins <mullinsjulian@gmail.com>
2016-06-11 14:14:28 -0400

    jQuery practice

---
+++ day6/problem_1_scaffold/main.js
@@ -0,0 +1,16 @@
+var list = $('ul > li');
+var span = $('span');
+var clicked;
+
+list.on('click', function(evt) {
+	span.empty();
+	var number;
+	for(var i=0; i<list.length; i++) {
+		if(evt.target === list[i]) {
+			clicked = list[i];
+			number = i;
+		}
+	}
+	console.log(clicked);
+	span.append("<h4>You clicked on: "+ clicked.innerHTML +", index #"+number+" of the list </h4>")
+})

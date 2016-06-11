29cf963f388de9d1c16625f13230c3dc31e3b4ac
AndyMartin93 <amarti40@villanova.edu>
2016-06-11 11:02:24 -0400

    keys and stuff

--- day4/2_horello-ajax/skeleton/js/config.js
+++ day4/2_horello-ajax/skeleton/js/config.js
@@ -2,6 +2,6 @@
 
 window.horello = window.horello || {};
 
-horello.apiKey = "YOUR API KEY HERE";
-horello.apiToken = "YOUR API TOKEN HERE";
+horello.apiKey = "9270a9f78a39cbcf01d3d807f4adc065";
+horello.apiToken = "28c68ee9537ca0f7d7d310f0a7cec8de54a2aceea4fa230d655e61f84a5b4f01";
 horello.apiUrl = "https://api.trello.com/1";

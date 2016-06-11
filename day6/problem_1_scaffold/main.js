95de4daf1e36671e0041677647895f47241a071b
spark4 <spark4@wellesley.edu>
2016-06-11 12:14:46 -0400

    darwish ruins lives

--- day6/problem_1_scaffold/main.js
+++ day6/problem_1_scaffold/main.js
@@ -1,21 +1,14 @@
 
-render = function(ind) {
-  // build wrappers
-  var wrapper = $('<div></div>');
-  var textwrapper = $('<div class="txt"></div>');
-  var text = $('<div class="txt-body" txt-id = "'+ind+'">That was div index'+ind+'</div>');
-
-  wrapper.append(textwrapper);
-  textwrapper.append(text);
-
-  return wrapper.html();
-};
-
 // on clicking, you should print out the index on to the page
+  
 
- $('.names').click(function(evt){
-    var index = $(evt.target).attr('ind');
-    var html = render(index);
-    $('#txt-insert').empty();
-    $('#txt-insert').append(html);
+  var arr = $('.names');
+
+  $('.names').click(function(evt){
+    var name = evt.target.innerHTML;
+    for (var i = 0; i<arr.length; i++) {
+      if (arr[i].innerHTML === name) {
+        document.getElementById('txt-insert').innerHTML = '<div>This div index is:'+i+'</div';
+      }
+    }
   });

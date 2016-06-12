240658501683751f74ba7ab28e3fbde09da4200f
tconcannon <taycon@seas.upenn.edu>
2016-06-12 17:22:01 -0400

    formatting

--- day6/problem_2_scaffold/main.js
+++ day6/problem_2_scaffold/main.js
@@ -13,15 +13,20 @@ $('.form-control').click(function(e){
 //push the submit form to create a new card 
 $('.submit').click(function(e){
 	if($.trim($('#text-input').val())===""){
-		alert("Need to tweet!")
+		alert("Need to tweet!");
 	}
 		//create the tweet
-		$('.list-container').prepend($('<div class = "center-block tweeted"><span class = "icon">\
-		<img src = "https://img.buzzfeed.com/buzzfeed-static/static/imagebuzz\
+		//new date object for date input
+		var x = new Date();
+		var months =['Jan','Feb','Mar','Apr','May','Jun','Jul','Sep','Oct','Nov','Dec'];
+		$('.list-container').prepend($('<div class = "center-block tweeted">\
+		<img class = "icon" src = "https://img.buzzfeed.com/buzzfeed-static/static/imagebuzz\
 		/terminal01/2010/1/26/14/bear-with-foot-long-tongue-14326-1264534270-16.jpg"\
-		height="100" width="100"></span><span class="msg">'+$('#text-input').val()+'</div>'))
+		height="100" width="100"><span class="msg">'+$('#text-input').val()+'</span>\
+		<span class ="date">'+x.getDate()+" "+months[x.getMonth()]+'</span>\
+		<span class = "user"><b>Taylor</b> @taylor_concannon</span>\
+		<span class= "expand">Expand</span></div>'));
 		
 		//clear out the text field
-		$.trim($('#text-input').val("")
-			console.log(Date(dateStr))
+		$('#text-input').val("");
 })

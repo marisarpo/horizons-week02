b9296459f27fbef6cc0027f867b827dd75cf4fc0
tconcannon <taycon@seas.upenn.edu>
2016-06-12 15:42:34 -0400

    text box expands and items display

--- day6/problem_2_scaffold/main.js
+++ day6/problem_2_scaffold/main.js
@@ -1,5 +1,26 @@
-//click the type:
+var wrapper = $('<div class = "container list-container"></div>');
+
+//click the type bo to expand and open buttons/stats:
 $('.form-control').click(function(e){
-	console.log('clicked')
-	$('.form-control').css("height","60px");
+	if($('#text-input').hasClass("open")){
+		$('#text-input').removeClass("open")
+		return;
+	}
+	$('#text-input').addClass("open")
 })
+
+//push the submit form to create a new card 
+$('.submit').click(function(e){
+	if($.trim($('#text-input').val())===""){
+		alert("Need to tweet!")
+	}
+	console.log('click')
+	wrapper.append($('<div class = "container tweet-box"><span><p>'+$('#text-input').val()+'</p></span></div>'))
+})
+
+
+
+
+
+///rendering: 
+//var wrapper = $('<div><div class = "container list-container"></div></div>');

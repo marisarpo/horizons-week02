c0b9ac24130a9ddf7ad1c1258166789874f1aac6
tconcannon <taycon@seas.upenn.edu>
2016-06-12 16:39:37 -0400

    tweets load and stack

--- day6/problem_2_scaffold/main.js
+++ day6/problem_2_scaffold/main.js
@@ -1,4 +1,5 @@
-var wrapper = $('<div class = "container list-container"></div>');
+var board = $('<div><div class= "board"><div id="boardAnchor"</div></div>')
+var wrapper = $('<div><div class = "container list-container"></div></div>');
 
 //click the type bo to expand and open buttons/stats:
 $('.form-control').click(function(e){
@@ -14,13 +15,13 @@ $('.submit').click(function(e){
 	if($.trim($('#text-input').val())===""){
 		alert("Need to tweet!")
 	}
-	console.log('click')
-	wrapper.append($('<div class = "container tweet-box"><span><p>'+$('#text-input').val()+'</p></span></div>'))
+		//create the tweet
+		$('.list-container').prepend($('<div class = "center-block tweeted"><span class = "icon">\
+		<img src = "https://img.buzzfeed.com/buzzfeed-static/static/imagebuzz\
+		/terminal01/2010/1/26/14/bear-with-foot-long-tongue-14326-1264534270-16.jpg"\
+		height="100" width="100"></span><span class="msg">'+$('#text-input').val()+'</div>'))
+		
+		//clear out the text field
+		$.trim($('#text-input').val("")
+			console.log(Date(dateStr))
 })
-
-
-
-
-
-///rendering: 
-//var wrapper = $('<div><div class = "container list-container"></div></div>');

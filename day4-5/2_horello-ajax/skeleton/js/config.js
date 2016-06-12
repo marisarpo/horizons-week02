a0fbb7abdea6c021f9d9a716a7bdd023853856f1
quidkid <isabella.gong@gmail.com>
2016-06-12 12:42:51 -0400

    reorganizing folder structure that got screwed up

--- day4-5/2_horello-ajax/skeleton/js/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-window.horello = window.horello || {};
-
-horello.apiKey = "63aa587ffd2bc71922dbbbdf72958ded";
-horello.apiToken = "a347b3f17d5ca9479ee565757c0e5a80661b163dd7e2e67aaa7af535e4cd73f7";
-horello.apiUrl = "https://api.trello.com/1";

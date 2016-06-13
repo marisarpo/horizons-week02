c340de1825e390481b9bc61c757647ff34e72444
Ying-Hang Eng <eyinghang@gmail.com>
2016-06-13 09:50:25 -0400

    twitter core functions done

--- day6/problem_2_scaffold/execution.js
+++ day6/problem_2_scaffold/execution.js
@@ -1,14 +1,15 @@
 twitter.mountStatic = function(){
 	$('#tweetSubmit').click(function(e) {
     var tweetText = $('#tweetText').val();
+    console.log(tweetText);
     // validate input
     if (!tweetText) {
       alert("Please enter something");
       return;
     }
-    list.addCart(tweetText);
+    list.addTweet(tweetText);
     $('#tweetText').val('');
-    twitter.mount();
+    twitter.mount(list);
   });
 };
 

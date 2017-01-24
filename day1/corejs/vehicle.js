603d8bfc0e3c4dedb1c3d3ca0f11ee66f0f1cb49
Rajiv Patel-O'Connor <rajiv.patel.oconnor@gmail.com>
2017-01-24 09:19:14 -0800

    week2day1

--- day1/corejs/vehicle.js
+++ day1/corejs/vehicle.js
@@ -5,8 +5,12 @@
 // model, year, color, tireType. The constructor should store the properties in
 // an object whenever it is called with the new keyword.
 
-var Vehicle // = YOUR CODE HERE
-
+var Vehicle = function(model, year, color, tireType){
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.tireType = tireType;
+  }
 // Now, let's create a few vehicle objects using the Vehicle constructor. Use new
 // and pass in any arguments for the instances. We have defined two types of tires
 // for you to use when creating the vehicles.
@@ -17,11 +21,12 @@ var allSeasonTires = {type: "all season"};
 // Create a "teslaS" variable that contains a new vehicle called "teslaS" that has
 // "snowTires" and a "red" color.
 
-var teslaS // =  YOUR CODE HERE
+var teslaS = new Vehicle("modelX", 2016, "red", snowTires)
+
 
 // Create a "jeep" variable that contains a new vehicle called "jeep" that has "allSeasonTires" and a "white" color.
 
-var jeep // =  YOUR CODE HERE
+var jeep = new Vehicle('jeep', 2010, 'yellow', allSeasonTires)
 
 // Now that you have two instances of vehicles, you want to add a couple of methods
 // to them. The first method is "changeColor", which receives a "color" argument.
@@ -32,10 +37,13 @@ var jeep // =  YOUR CODE HERE
 // jeep.changeColor("blue")
 // And it should change the object vehicles's color.
 
-// YOUR CODE HERE
+Vehicle.prototype.changeColor = function(color){
+  this.color = color;
+}
 
 // The second method we want to add is "whichTires". This method should return
 // the type of tires the vehicle has.
 // It should only be available to "jeep".
-
-// YOUR CODE HERE
+jeep.whichTires = function(){
+  return this.tireType;
+}

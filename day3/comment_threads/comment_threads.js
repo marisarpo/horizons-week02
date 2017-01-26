49b6a6c09385cb7b457fccd5eaedd0989e50102e
andrewkmin <andrewmin8@gmail.com>
2017-01-26 13:29:46 -0800

    day3

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,64 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$('.post').on('click', function() {
+  var author = prompt('Enter your name');
+  var comment = prompt('Enter your comment');
+
+  var newElement = '<div class=' + '"comment">' + '<div class="author">' + author +
+     ' says: &nbsp</div>' + '<div class="message">' + comment + '</div>' + '<div class="controls">' +
+     '<button class="hide-replies btn btn-default">' + 'Hide Replies</button>' +
+     '<button class="show-replies btn btn-default">' + 'Show Replies</button>' +
+     '<button class="reply btn btn-default">' + 'Reply</button>' +
+     '</div>' + '<div class="replies">' + '</div>' + '</div>';
+
+$('.comments').append(newElement);
+});
+
+// STEP 2
+$('.comments').on('click', '.reply.btn.btn-default', function() {
+  var author = prompt('Enter your name');
+  var comment = prompt('Enter your comment');
+
+  var newElement = '<div class=' + '"comment">' + '<div class="author">' + author +
+     ' says: &nbsp</div>' + '<div class="message">' + comment + '</div>' + '<div class="controls">' +
+     '<button class="hide-replies btn btn-default">' + 'Hide Replies</button>' +
+     '<button class="show-replies btn btn-default">' + 'Show Replies</button>' +
+     '<button class="reply btn btn-default">' + 'Reply</button>' +
+     '</div>' + '<div class="replies">' + '</div>' + '</div>';
+
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(newElement);
+  repliesDiv.show();
+  return false;
+
+});
+
+
+// STEP 3
+$('.comments').on('click', '.hide-replies.btn.btn-default', function() {
+
+  //var $this = $(this); // the comment on which we're clicking hide reply
+  var commentDiv = $(this).closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+
+  var sibling = $(this).siblings('.reply');
+  sibling.hide();
+  repliesDiv.hide();
+
+});
+
+
+$('.comments').on('click', '.show-replies.btn.btn-default', function() {
+
+  //var $this = $(this); // the comment on which we're clicking hide reply
+  var commentDiv = $(this).closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+
+  var sibling = $(this).siblings('.reply');
+  sibling.show();
+  repliesDiv.show();
+
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,103 @@
 "use strict";
 
-// YOUR JAVASCRIPT CODE GOES HERE
+// GLOBAL VARIABLE PART 4
+var cardBeingCalled = null;
+
+
+$('.list-cards').sortable({
+  connectWith: ".list-cards"
+}).disableSelection();
+
+// PART 4 CLICK LISTENER;; WE ARE NOT DONE HERE YET
+$('.board').on('click', '.card', function() {
+  cardBeingCalled = $(this);
+  var modal = $('#card-edit').modal();
+  var ourBody = $(this).find(".card-body").text();
+  $("#card-edit-body").val(ourBody);
+
+})
+
+$('.modal-footer').on('click', '.card-edit-save', function() {
+  var newText = $("#card-edit-body").val();
+  $(cardBeingCalled).find(".card-body").text(newText);
+  $('.modal').modal('toggle');
+});
+
+
+
+// add list button
+$('.board').on('click', '.add-list', function() {
+  $('.add-list-form-wrapper').removeClass('collapse');
+});
+
+
+
+
+$('.board').on('click', '.add-list-cancel', function() {
+  $('.add-list-form-wrapper').addClass('collapse');
+});
+
+
+
+$('.board').on('click', '.add-list-save', function() {
+  var myInput = $(this).siblings("input").val();
+  $(this).siblings("input").val(null);
+  var $this = $(this);
+
+  var newList = '<div class=' + '"list-container">' +
+  '<div class="list">' + '<div class="list-header">' +
+  '<span class = "list-title">' + myInput + '</span>' +
+  '</div>' + '<div class = "list-cards">' + '</div>' +
+  '<div class = "list-footer">' + '<button class= "add-card">'
+  + 'Add a card' + '</button>' +
+  '<div class = "collapse add-card-form-wrapper">' +
+  '<div class = "well add-card-form">' +
+  '<input type="text" class="form-control" placeholder="Card title">' +
+  '<button type="button" class="btn btn-default add-card-save">' + 'Save' +
+  '</button>' + '<button type="button" class="btn btn-default add-card-cancel">' +
+  '<span class="glyphicon glyphicon-remove">' + '</span>' + '</button>' +
+  '</div>' + '</div>' + '</div>'+ '</div>' + '</div>';
+
+  $('.add-list-container').parent().before(newList);
+  $('.add-list-form-wrapper').addClass('collapse');
+
+  // PART 5
+  $('.list-cards').sortable({
+    connectWith: ".list-cards"
+  }).disableSelection();
+
+// doesn't work: board, list-container, list,
+
+});
+
+
+// CARDS
+
+$('.board').on('click', '.add-card', function() {
+  var $this = $(this);
+  // $('.add-card-form-wrapper').removeClass('collapse');
+  $this.siblings('.add-card-form-wrapper').removeClass('collapse');
+});
+
+//////////
+
+$('.board').on('click', '.add-card-cancel', function() {
+  var $this = $(this);
+  $this.closest('.add-card-form-wrapper').addClass('collapse');
+});
+
+////////
+
+$('.board').on('click', '.add-card-save', function() {
+  var myCardInput = $(this).siblings("input").val();
+  $(this).siblings("input").val(null);
+  var $this = $(this);
+
+  var newCardElement = '<div class="card">' + '<span class="card-more">'
+  + '<span class="glyphicon glyphicon-align-left"></span>' +
+  '</span>' + '<div class = "card-body">' + myCardInput + '</div>' +
+  '</div>';
+
+  $this.closest('.list-footer').siblings('.list-cards').append(newCardElement);
+  $this.closest('.add-card-form-wrapper').addClass('collapse');
+});

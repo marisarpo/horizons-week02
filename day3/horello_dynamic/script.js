336c54a703f7dbb9e050dc698343917a786b9d0b
Syed Mohsin <syedm.90@gmail.com>
2017-01-25 17:21:31 -0800

    complete horello jquery responses

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// GLOBAL VARIABLES
+var cardBeingEdited = null;
+
 // YOUR JAVASCRIPT CODE GOES HERE
 $("button.add-list").on("click", function(e) {
   $(this).parent().children(".add-list-form-wrapper").removeClass('collapse');
@@ -14,10 +17,14 @@ $("button.add-list-save").on("click", function(e) {
 
   var title = $(this).parent().children(".form-control").val();
   var newList = addList(title);
-  $(".list-container:nth-last-child(1)").insertBefore(newList);
+  $(".list-container:last-child").before(newList);
 
+  $('.list-cards').sortable( {
+    revert: false,
+    items: '.card',
+    connectWith: '.list-cards'
+  });
 
-  // $(this).closest(".board").append(newList);
 });
 
 function addList(title) {
@@ -45,6 +52,53 @@ function addList(title) {
   </div>');
 }
 
+function addCard(title) {
+  return $('<div class="card">\
+              <span class="card-more">\
+                <span class="glyphicon glyphicon-align-left"></span>\
+              </span>\
+              <div class="card-body">' + title + '</div>\
+            </div>`'
+          );
+}
 
+$('.board').on('click', '.add-card', function(e) {
+  $(this).parent().children('.add-card-form-wrapper').removeClass('collapse');
+});
 
-// $(this).closest(".board").append("");
+$('.board').on('click', '.add-card-cancel', function(e) {
+  $(this).parent().closest('.list-footer').children('.add-card-form-wrapper').addClass('collapse');
+});
+
+$('.board').on('click', '.add-card-save', function(e) {
+  var title = $(this).parent().children(".form-control").val();
+  var newCard = addCard(title);
+
+  $(this).closest('.list').children('.list-cards').append(newCard);
+});
+
+$('.board').on('click', '.card', function(e) {
+  cardBeingEdited = this;
+  console.log(this);
+  $('#card-edit').modal();
+  var curText = $(this).children('.card-body').text();
+  $('#card-edit-body').val(curText);
+});
+
+$('.card-edit-save').on('click', function(e){
+  var newText = $('#card-edit-body').val();
+  $(cardBeingEdited).children('.card-body').text(newText);
+  cardBeingEdited = null;
+  $('#card-edit').modal('hide');
+});
+
+$('.card-edit-cancel').on('click', function(e) {
+  cardBeingEdited = null;
+  $('#card-edit').modal();
+});
+
+$('.list-cards').sortable( {
+  revert: false,
+  items: '.card',
+  connectWith: '.list-cards'
+});

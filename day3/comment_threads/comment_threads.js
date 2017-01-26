449a89e8bc78c350f8c27bd581e9d4b670dfd3e3
lisaztruong <lisaztruong@gmail.com>
2017-01-25 16:05:59 -0800

    work on day 3

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,71 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+// <button class="post btn btn-default">Post Comment</button>
+
+// post comment button
+$('.post.btn.btn-default').on('click', function() {
+  var author = prompt("Enter your name:");
+  var comment = prompt("Enter your comment:")
+
+  var html = '<div class="comment">' +
+    '<div class="author">"' + author + '" says:</div>' +
+    '<div class="message">' + comment + '</div>' +
+    '<div class="controls">' +
+      '<button class="hide-replies btn btn-default">Hide Replies</button>' +
+      '<button class="show-replies btn btn-default">Show Replies</button>' +
+      '<button class="reply btn btn-default">Reply</button>' +
+    '</div>' +
+    '<div class="replies"></div>' +
+  '</div>'
+
+  $('.comments').append(html);
+});
+
+
+// reply buttons
+$('.comments').on('click', '.reply', function() {
+  // `this` points to the current `.reply` button that was clicked
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+
+  var author = prompt("Enter your name:");
+  var comment = prompt("Enter your comment:");
+
+  var html = '<div class="comment">' +
+    '<div class="author">"' + author + '" says:</div>' +
+    '<div class="message">' + comment + '</div>' +
+    '<div class="controls">' +
+      '<button class="hide-replies btn btn-default">Hide Replies</button>' +
+      '<button class="show-replies btn btn-default">Show Replies</button>' +
+      '<button class="reply btn btn-default">Reply</button>' +
+    '</div>' +
+    '<div class="replies"></div>' +
+  '</div>'
+
+  repliesDiv.append(html);
+});
+
+// hide buttons
+$('.comments').on('click', '.hide-replies',
+ function() {
+   var $this = $(this); // this refer to the class "comment"
+   var commentDiv = $this.closest('.comment'); // parent of comment is comments -> find "comment"
+   var repliesDiv = commentDiv.children('.replies'); //replies are children of Comment -> find children
+
+   repliesDiv.hide();
+ });
+
+// show replies
+ $('.comments').on('click', '.show-replies',
+   function() {
+     var $this = $(this); // this refer to the class "comment"
+     var commentDiv = $this.closest('.comment'); // parent of comment is comments -> find "comment"
+     var repliesDiv = commentDiv.children('.replies'); //replies are children of Comment -> find children
+
+
+   repliesDiv.show();
+
+   });
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,15 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+
+  $('.add-list').on('click', function(){
+    $('.add-list-form-wrapper').removeClass('collapse');
+  });
+
+  $('.add-list-cancel').on('click', function(){
+    $('.add-list-form-wrapper').addClass('collapse');
+  })
+
+  $('.add-list-save').on('click', function(){
+    console.log(input.val());
+  })

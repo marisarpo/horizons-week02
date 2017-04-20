cbf11145de8ad8531d0f3ca65b156e0b28d30a8a
Prathmesh Desai <d.prathmesh@gmail.com>
2017-04-20 11:26:06 -0700

    code review changes

--- day4/2_horello-ajax/src/solution/js/config.js
+++ day4/2_horello-ajax/src/solution/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "ac960adda2eff64acbbd98e795c025ca";
-var apiToken = "4f056833805f0d9f9942665e386fb731be47e335e7b9a32a5abb87e1e2ea8125";
+var apiKey = "YOUR API KEY HERE";
+var apiToken = "YOUR API TOKEN HERE";
 var apiUrl = "https://api.trello.com/1";
-var boardId = "588577bb8423080722cabe8c"
+var boardId = "YOUR BOARD ID HERE"

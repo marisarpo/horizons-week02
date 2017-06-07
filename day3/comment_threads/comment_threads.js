c1d7a961fc4ea5908e00c629d033928b84c8f23d
maggie <hcc375@nyu.edu>
2017-06-07 14:59:30 -0700

    update

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,60 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+$(".post").on('click', function(){
+  var name = prompt("Enter your name", "username")
+  var comment = prompt("Enter your comment", "comments")
+  // $('.author').val(name);
+  // $('.comment').eq(4).append('<div class="author">' + name + '</div>'
+  var newComment = `<div class="comment">
+    <div class="author">${name} says:</div>
+    <div class="message">${comment}</div>
+    <div class="controls">
+      <button class="hide-replies btn btn-default">Hide Replies</button>
+      <button class="show-replies btn btn-default">Show Replies</button>
+      <button class="reply btn btn-default">Reply</button>
+    </div>
+    <div class="replies"></div>
+  </div>`;
+
+
+  $('.comments').append(newComment)
+
+})
+
+$('.comments').on('click', '.reply', function(){
+  var name = prompt("Enter your name", "username")
+  var comment = prompt("Enter your comment", "comments")
+
+  var newComment = `<div class="comment">
+    <div class="author">${name} says:</div>
+    <div class="message">${comment}</div>
+    <div class="controls">
+      <button class="hide-replies btn btn-default">Hide Replies</button>
+      <button class="show-replies btn btn-default">Show Replies</button>
+      <button class="reply btn btn-default">Reply</button>
+    </div>
+    <div class="replies"></div>
+  </div>`;
+
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment')
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(newComment);
+})
+
+$('.comments').on('click', '.hide-replies', function(){
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.hide();
+
+})
+$('.comments').on('click', '.show-replies', function(){
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.show();
+
+})

39054936a793884a562bb019ab0331831d72794e
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-06-07 14:12:30 -0700

    finished task one, just for you danny

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,24 +1,35 @@
 "use strict";
 $(document).ready(function(){
+$('.show-replies').hide();
 
 $('.post').on('click',function(){
-prompt('Enter your name: ');
-prompt('Enter your comment: ');
-$('<div class="comment">
-  <div class="author">"AUTHOR NAME HERE" says:</div>
-  <div class="message">COMMENT HERE</div>
-  <div class="controls">
-    <button class="hide-replies btn btn-default">Hide Replies</button>
-    <button class="show-replies btn btn-default">Show Replies</button>
-    <button class="reply btn btn-default">Reply</button>
-  </div>
-  <div class="replies"></div>
-</div>')
+var name = prompt('Enter your name: ');
+var response = prompt('Enter your comment: ');
+var newElement = $('<div class="comment"><div class="author">'+ '"' + name +'"' +' says:</div><div class="message">'+ response +'</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+$(".comments").append(newElement);
 })
 
+$('.comments').on('click','.reply',function(){
+var name = prompt('Enter your name: ');
+var response = prompt('Enter your comment: ');
+var newElement = $('<div class="comment"><div class="author">'+ '"' + name +'"' +' says:</div><div class="message">'+ response +'</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+console.log($(this))
+$(this).parent().parent().children('.replies').append(newElement);
+})
 
+$('.comments').on('click','.hide-replies',function(){
+$(this).parent().parent().children('.replies').hide();
+$(this).hide();
+$(this).parent().children('.show-replies').show();
+var commentCounter = $(this).closest('.comment').find('.comment').length;
+$(this).closest('.comment').append($('<span>'+commentCounter+' comments hidden'+'</span>'))
+})
 
+$('.comments').on('click','.show-replies',function(){
+$(this).parent().parent().children('.replies').show();
+$(this).hide();
+$(this).parent().children('.hide-replies').show();
+$(this).closest('.comment').children('span').hide();
+})
 
 })
-// This file contains JavaScript that will run on your page.
-// YOUR CODE HERE

70f0a61674c39ab09b286c62806d5c45fd94760b
Oberon <ahijaouy@gatech.edu>
2017-06-07 13:32:49 -0700

    post a comment

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,30 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$(document).ready(function() {
+
+  //Post Comment Event Listener
+  $(".post").on("click", function() {
+    var author = prompt("Enter your name");
+    var comment = prompt("Enter your comment");
+
+    //build DOM Element
+    var post = $(`
+      <div class="comment">
+        <div class="author">"` + author + `" says:</div>
+        <div class="message">` + comment + `</div>
+        <div class="controls">
+          <button class="hide-replies btn btn-default">Hide Replies</button>
+          <button class="show-replies btn btn-default">Show Replies</button>
+          <button class="reply btn btn-default">Reply</button>
+        </div>
+        <div class="replies"></div>
+      </div>`);
+
+    //Append to the Comments Div
+    $(".comments").append(post);
+  });
+
+
+  //
+});

fff027e2c541e29f5f7ae7352be1e7ea2277792c
yaruliu <yarusophia.liu@mail.utoronto.ca>
2017-06-07 13:00:33 -0700

    Jun7 modify

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,40 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+
+$('.post').click(function() {
+  // var author = prompt("Please enter an author")
+  // var comment = prompt("Please enter a comment")
+
+  $('.comment').append(promptInfo());
+})
+
+function promptInfo() {
+  var author = prompt("Please enter an author")
+  var comment = prompt("Please enter a comment")
+  console.log(author, comment);
+  if (author && comment) {
+    return commentInfo(author, comment);
+  }
+}
+
+function commentInfo(author, comment, reply) {
+  var comment = $('<div class="comment">');
+  comment.append($('<div class="author">').text('"' + author + '" says:'));
+  comment.append($('<div class="message">').text(comment));
+  var controls = $('<div class="controls">')
+  controls.append($('<button class="hide-replies btn btn-default">Hide Replies</button>'));
+  controls.append($('<button class="show-replies btn btn-default">Show Replies</button>'));
+  controls.append($('<button class="reply btn btn-default">Reply</button>'));
+  comment.append(controls);
+  comment.append($(<div class="replies">).append(reply));
+  return comment;
+}
+
+$('.comment').on('click', '.reply', function() {
+  var $this = $(this);
+  var reply = $this.closest('.comment').children('.replies')
+  reply.show();
+  reply.append(promptInfo());
+})

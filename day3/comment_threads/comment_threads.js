12d0e77e1e230767b60d1427daf37c3578748910
ChiefTeef <laa6@rice.edu>
2017-06-08 13:19:52 -0700

    idk

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,41 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+var name;
+var blurb;
+var rname;
+var rblurb;
+var newcommen1 = "<div class=\"comment\"> <div class=\"author\">\""
+var newcommen2 = "\" says:</div> <div class=\"message\">"
+var newcommen3 = "</div> <div class=\"controls\"> <button class=\"hide-replies btn btn-default\">Hide Replies</button><button class=\"show-replies btn btn-default\">Show Replies</button> <button class=\"reply btn btn-default\">Reply</button></div><div class=\"replies\"></div></div>"
+
+$(".post").click( function(){
+  name = prompt("What is your name?");
+  blurb = prompt("Comment");
+  $("div .comments").append(newcommen1 + name + newcommen2 + blurb + newcommen3);
+})
+
+// $(".reply").click( function(){
+//   rname = prompt("What is your name?");
+//   rblurb = prompt("Comment");
+//   $(this).append(newcommen1 + rname + newcommen2 + rblurb + newcommen3);
+// })
+
+
+
+$(".comments").on('click', '.reply', function(){
+  rname = prompt("What is your name?");
+  rblurb = prompt("Comment");
+  $(this).parent().parent().children(".replies").append(newcommen1 + rname + newcommen2 + rblurb + newcommen3);
+})
+
+$(".comments").on('click', '.hide-replies', function(){
+
+  $(this).parent().parent().children(".replies").hide();
+  $(this).parent().parent().children("hide-replies").collapse();
+
+})
+
+$(".comments").on('click', '.show-replies', function(){
+  $(this).parent().parent().children(".replies").show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,76 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+var title2;
+var p1 = `<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title">`
+var p2 = `</span>
+          </div>
+          <div class="list-cards"></div>
+          <div class="list-footer">
+            <button class="add-card">Add a card...</button>
+            <div class="collapse add-card-form-wrapper">
+              <div class="well add-card-form">
+                <input type="text" class="form-control" placeholder="Card title">
+                <button type="button" class="btn btn-default add-card-save">
+                  Save
+                </button>
+                <button type="button" class="btn btn-default add-card-cancel">
+                  <span class="glyphicon glyphicon-remove"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+var c1 = `<div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">`
+
+var c2 = `</div>
+</div>`
+
+$(".add-list").click( function(){
+$('.add-list-form-wrapper').removeClass('collapse')
+})
+
+$(".add-list-cancel").click( function(){
+$('.add-list-form-wrapper').addClass('collapse')
+})
+
+var title;
+$(".add-list-save").click( function(){
+ title = $('.add-list-form-wrapper').find('.form-control').val();
+$(this).closest('.list-container').before(p1 + title + p2)
+$('.add-list-form-wrapper').addClass('collapse')
+})
+
+$('.board').on('click', '.add-card', function() {
+  $(this).siblings().removeClass('collapse')
+})
+
+$('.board').on('click', '.add-card-cancel', function() {
+  $(this).parent().parent().addClass('collapse')
+
+})
+
+$('.board').on('click', '.add-card-save', function() {
+title2 = $(this).siblings('.form-control').val();
+$(this).closest('.list-footer').siblings('.list-cards').append(c1 + title2 + c2)
+$('.add-card-form-wrapper').addClass('collapse')
+})
+
+var cardBeingEdited = null;
+
+$('.board').on('click', '.card', function() {
+  cardBeingEdited = $(this)
+
+// #card-edit-body
+
+$('#card-edit-body').val(cardBeingEdited.find('.card-body').text())
+  $("#card-edit").modal()
+})

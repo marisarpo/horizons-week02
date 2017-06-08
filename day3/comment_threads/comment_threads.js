735de1e3fe51e60eff8511b22a4cf371d59610b0
sophiagrace <sophia.torrance@pomona.edu>
2017-06-08 14:45:34 -0700

    add day 3 work and day 4 morning exercises

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,55 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$('.comments').on('click', '.hide-replies', function(){
+	// debugger;
+	$(this).parent().siblings('.replies').hide();
+
+})
+$('.comment').on('click', '.show-replies', function(){
+	$(this).parent().siblings('.replies').show();
+})
+
+
+$('.comments').on('click', '.post', function(){
+	var mytext = (prompt('enter your text'));
+	if(mytext == '') {
+		alert('please enter text for your comment');
+	}
+	else{
+		$(this).siblings('.comments').append(`
+          	<div class="message comment"> 
+          		<div class="author"> "Anon" says: </div>` 
+          		+ mytext + 
+          		`<div class="controls">
+          			<button class="hide-replies btn btn-default"> Hide Replies </button>
+          			<button class="show-replies btn btn-default"> Show Replies </button>
+          			<button class="reply btn btn-default"> Reply </button>
+          		</div>
+        	</div>`
+        );
+	}
+})
+
+$('.comments').on('click', '.reply', function() {
+	var myReply = (prompt('enter your reply'));
+	if(myReply == '') {
+		alert('please enter text for your reply');
+	}
+	else{
+		$(this).parent().siblings('.replies').append(`
+			<div class="comment message">
+          		<div class="author"> "Anon" says: </div>` 
+          		+ myReply + 
+          		`<div class="controls">
+          			<button class="hide-replies btn btn-default"> Hide Replies </button>
+          			<button class="show-replies btn btn-default"> Show Replies </button>
+          			<button class="reply btn btn-default"> Reply </button>
+          			</div>
+          			<div class="replies">
+          		</div>
+        	</div>`
+        )
+	}
+})	
+
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,86 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+$('.add-list').on('click', function(){
+	$(this).siblings().removeClass('collapse');
+})
+
+$('.add-list-cancel').on('click', function(){
+	$(this).siblings('.form-control').val('');
+	$(this).parent().parent().addClass('collapse');
+})
+
+$('.add-list-save').on('click', function(){
+	var title = $(this).siblings('.form-control').val();
+	var extraList = $(`<div class="list-container">
+  			<div class="list">
+    			<div class="list-header">
+      				<span class="list-title">` + title + `</span>
+    			</div>
+    			<div class="list-cards"></div>
+    			<div class="list-footer">
+      				<button class="add-card">Add a card...</button>
+      				<div class="collapse add-card-form-wrapper">
+        				<div class="well add-card-form">
+          					<input type="text" class="form-control" placeholder="Card title">
+         					<button type="button" class="btn btn-default add-card-save"> Save </button>
+          					<button type="button" class="btn btn-default add-card-cancel">
+            					<span class="glyphicon glyphicon-remove"></span>
+          					</button>
+        				</div>
+      				</div>
+    			</div>
+  			</div>
+		</div>`)
+	$('.add-list-container').parent().before(extraList);
+	$( ".list-cards" ).sortable({
+  		appendTo: '.list-container',
+  		connectWith: '.list-cards'
+	});
+	$(this).siblings('.form-control').val('')
+	$(this).parent().parent().addClass('collapse');
+})
+
+$('.board').on('click', '.add-card', function(){
+	$(this).siblings().removeClass('collapse');
+})
+
+$('.board').on('click', '.add-card-cancel', function(){
+	$(this).parent().parent().addClass('collapse');
+	$(this).siblings('.form-control').val('');
+})
+
+$('.board').on('click', '.add-card-save', function(){
+	var cardTitle = $(this).siblings('.form-control').val();
+	var extraCard = $(`<div class="card">
+  			<span class="card-more">
+    			<span class="glyphicon glyphicon-align-left"></span>
+  			</span>
+  		<div class="card-body">`+ cardTitle + `</div>
+	</div>`)
+	$(this).parent().parent().parent().siblings('.list-cards').append(extraCard)
+	$(this).siblings('.form-control').val('')
+	$(this).parent().parent().addClass('collapse');
+})
+
+var cardBeingEdited = '';
+
+$('.board').on('click', '.card', function(){
+	cardBeingEdited = this;
+	var oldText = $(cardBeingEdited).children('.card-body').text();
+	$('#card-edit-body').val(oldText);
+	$('#card-edit').modal();	
+})
+
+$('.card-edit-save').on('click', function(){
+	var editText = $('textarea').val();
+	$(cardBeingEdited).children('.card-body').text(editText);
+	$('#card-edit').modal('hide');
+})
+
+
+$( ".list-cards" ).sortable({
+  appendTo: '.list-container',
+  connectWith: '.list-cards'
+});
+

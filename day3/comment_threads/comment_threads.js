c939ead087dbcd8c5b20001054a52e36ca837b14
sdarchis <s.darchis@me.com>
2017-06-08 09:43:48 -0700

    Day 3 Week 2

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,37 @@
 "use strict";
-
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+// add the click handler
+
+$('.post').on('click', function() {
+  var name = prompt("Enter your name");
+  var comment = prompt("Enter your comment");
+  var commentDiv = $('<div class="comment"><div class="author">"' + name +  '" says: </div><div class="message">' + comment + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+  $('.comments').append(commentDiv);
+})
+$('.comment').on('click', '.reply', function() {
+  var name = prompt("Enter your name");
+  var comment = prompt("Enter your comment");
+  var commentDiv = $('<div class="comment"><div class="author">"' + name +  '" says: </div><div class="message">' + comment + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+  var $this = $(this);
+  var commentLoc = $this.closest('.comment');
+  var repliesLoc = commentLoc.children('.replies');
+  repliesLoc.append(commentDiv);
+})
+$('.comments').on('click', '.hide-replies', function(){
+  var $this = $(this);
+  var commentLoc = $this.closest('.comment');
+  var repliesLoc = commentLoc.children('.replies');
+  repliesLoc.hide();
+  $this.hide();
+  $this.closest('.comment').children('.controls').children('.show-replies').show();
+
+})
+$('.comments').on('click', '.show-replies', function(){
+  var $this = $(this);
+  var commentLoc = $this.closest('.comment');
+  var repliesLoc = commentLoc.children('.replies');
+  repliesLoc.show();
+  $this.hide();
+  $this.closest('.comment').children('.controls').children('.hide-replies').show();
+})

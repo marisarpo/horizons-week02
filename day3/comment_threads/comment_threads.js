98e180bf679cc1e0e6efb605a523244646f7d317
AMY JIN <zjin@wellesley.edu>
2017-06-08 09:22:06 -0700

    amyzjin

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,41 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+function addComment(){
+var userAuthor = prompt("Enter your name: ");
+var userComment = prompt("Enter your comment: ");
+
+var comment  = $('<div>').addClass('comment');
+var author = $('<div>').addClass('author').text(userAuthor+' says:');
+var message = $('<div>').addClass('message').text(userComment);
+var controls = $('<div>').addClass('controls');
+
+var buttonHide = $('<button>').addClass('hide-replies btn btn-default').text('Hide Replies');
+var buttonShow = $('<button>').addClass('show-replies btn btn-default').text('Show Replies');
+var reply = $('<button>').addClass('reply btn btn-default').text('Reply');
+
+var replies = $('<div>').addClass('replies');
+
+  controls.append(buttonHide).append(buttonShow).append(reply);
+  comment.append(author).append(message).append(controls).append(replies);
+  return comment;}
+
+
+$('.post').on('click',function(){
+  var comment = addComment();
+  $('.comments').append(comment);
+});
+
+$('.comments').on('click','.reply', function(){
+  var comment = addComment();
+  $(this).parent().siblings('.replies').append(comment);
+});
+
+$('.comments').on('click','.hide-replies', function(){
+  $(this).parent().siblings('.replies').hide();
+});
+
+$('.comments').on('click','.show-replies', function(){
+  $(this).parent().siblings('.replies').show();
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,86 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+$('.board').on('click','.add-list',function(){
+  $(this).siblings('.add-list-form-wrapper').removeClass('collapse');
+})
+
+$('.board').on('click','.add-list-cancel',function(){
+  $(this).parent('.add-card-form').parent().addClass('collapse');
+});
+
+$('.board').on('click','.add-list-save',function(){
+var userInput = $(this).siblings('.form-control').val();
+
+var listContainer = $('<div>').addClass('list-container');
+  var list = $('<div>').addClass('list');
+
+    var listHeader = $('<div>').addClass('list-header');
+      var listTitle = $('<span>').addClass('list-title').text(userInput);
+
+    var listCards = $('<div>').addClass('list-cards');
+    var listFooter = $('<div>').addClass('list-footer');
+        var addCard = $('<button>').addClass('add-card').text('Add a card');
+        var collapseAddCardFormWrapper = $('<div>').addClass('collapse add-card-form-wrapper');
+          var wellAddCardForm = $('<div>').addClass('well add-card-form');
+            var formControl = $('<input>').addClass('form-control').attr({
+              type:'text',
+              placeholder:'Card title'
+            })
+            var addCardSave = $('<button>').addClass('btn btn-default add-card-save').text('Save');
+            var addCardCancel = $('<button>').addClass('btn btn-default add-card-cancel');
+              var glyphicon = $('<span>').addClass('glyphicon glyphicon-remove');
+
+            addCardCancel.append(glyphicon);
+            wellAddCardForm.append(formControl).append(addCardSave).append(addCardCancel);
+            collapseAddCardFormWrapper.append(wellAddCardForm);
+            listFooter.append(addCard).append(collapseAddCardFormWrapper);
+            listHeader.append(listTitle);
+            list.append(listHeader).append(listCards).append(listFooter);
+            listContainer.append(list);
+
+            $(this).parent().parent().parent().parent('.list-container').before(listContainer);
+            $(this).parent().parent('.add-list-form-wrapper').hide();
+});
+
+$('.board').on('click','.add-card',function(){
+  $(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+    });
+
+$('.board').on('click','.add-card-cancel',function(){
+  $(this).parent().parent('.add-card-form-wrapper').addClass('collapse');
+});
+
+$('.board').on('click','.add-card-save',function(){
+  var userInput2 = $(this).siblings('.form-control').val();
+  console.log(userInput2);
+  var card = $('<div>').addClass('card');
+  var cardBody = $('<div>').addClass('card-body').text(userInput2);
+  var cardMore = $('<span>').addClass('card-more');
+  var g3= $('<span>').addClass('glyphicon glyphicon-align-left');
+  cardMore.append(g3);
+  card.append(cardMore).append(cardBody);
+
+  $(this).parent().parent().parent().siblings('.list-cards').append(card);
+  $(this).parent().parent('.add-card-form-wrapper').addClass('collapse');
+
+});
+
+
+var cardBeingEdited = null;
+
+$('.board').on('click','.card',function(){
+  cardBeingEdited=this;
+  $('#card-edit').modal();
+  $('#card-edit-body').text($(this).find('.card-body').text());
+});
+
+$('.card-edit-save').on('click',function(){
+  var text = $(this).closest('.modal-content').find('textarea').val();
+  console.log(text);
+  $(cardBeingEdited).children('.card-body').text(text);
+  $('#card-edit').modal('hide');
+
+});
+
+$('.list-cards').sortable();

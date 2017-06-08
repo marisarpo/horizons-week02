393777eeb0b9280994a1f26cd5ba1ee1b3d758e4
allaunyc <alau11@binghamton.edu>
2017-06-08 13:52:09 -0700

    day4 videos

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax ({
+  url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp){
+    var splitWords = resp.split(' ').length;
+    var count = $('#count').replaceWith(splitWords);
+    console.log(count);
+  }
+})

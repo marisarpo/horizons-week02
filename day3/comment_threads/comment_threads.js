1097e9cff0e85b3a83eaa8bb8349e83f20ad5157
jatparam <jatharsan.param@gmail.com>
2017-06-08 12:06:42 -0400

    update

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,70 @@
 "use strict";
 
 // This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+$( document ).ready(function() {
+
+
+
+    $(".post").on("click", "p", function() {
+      var authorName = prompt("please enter your name");
+      var ourComment = prompt("type comment");
+
+      var newComment = $('<div class="comment"> \
+      <div class="author">' + authorName +   'says:</div> \
+      <div class="message"> ' + ourComment + '</div> \
+      <div class="controls"> \
+      <button class="hide-replies btn btn-default">Hide Replies</button> \
+      <button class="show-replies btn btn-default">Show Replies</button> \
+      <button class="reply btn btn-default">Reply</button> \
+      </div> \
+      <div class="replies"></div> \
+      </div> \
+      ')
+
+      $('.comments').append(newComment)
+
+    });
+
+    $(".comments").on("click", ".reply", function() {
+      var authorName = prompt("please enter your name");
+      var ourComment = prompt("type comment");
+
+      var newComment = $('<div class="comment"> \
+      <div class="author">  'authorName' says:</div> \
+      <div class="message"> ' + ourComment + '</div> \
+      <div class="controls"> \
+      <button class="hide-replies btn btn-default">Hide Replies</button> \
+      <button class="show-replies btn btn-default">Show Replies</button> \
+      <button class="reply btn btn-default">Reply</button> \
+      </div> \
+      <div class="replies"></div> \
+      </div> \
+      ')
+
+
+      var commentDiv = $(this).closest('.comment');
+      var repliesDiv = commentDiv.children('.replies');
+
+      repliesDiv.append(newComment);
+
+
+    })
+
+    $(".comments").on("click", ".hide-replies", function() {
+      var $this = $(this);
+      var commentDiv = $this.closest('.comment');
+      var repliesDiv = commentDiv.children(".replies");
+      $(repliesDiv).hide();
+    })
+
+    $(".comments").on("click", ".show-replies", function() {
+      var $this = $(this);
+      var commentDiv = $this.closest('.comment');
+      var repliesDiv = commentDiv.children(".replies");
+      $(repliesDiv).show();
+    })
+
+
+
+
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,85 @@
 "use strict";
 
-// YOUR JAVASCRIPT CODE GOES HERE
+$( document ).ready(function() {
+
+
+  $(".add-list").on("click", function() {
+    $(".add-list-form-wrapper").removeClass("collapse")
+  });
+
+  $(".add-list-cancel").on("click", function() {
+    $(".add-list-form-wrapper").addClass("collapse")
+  });
+
+
+
+
+
+   $(".add-list-save").on("click", function() {
+    var written = $(".add-list-form-wrapper .form-control").val();
+
+    var currentContainer = $(this).closest('.list-container')
+    var newComment = $(`
+    <div class="list-container">
+      <div class="list">
+        <div class="list-header">
+          <span class="list-title"> ${written} </span>
+    </div>
+
+    <div class="list-cards"></div>
+
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+          Save
+    </button>
+
+    <button type="button" class="btn btn-default add-card-cancel">
+      <span class="glyphicon glyphicon-remove"></span>
+    </button>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    `)
+
+    currentContainer.before(newComment)
+  });
+
+
+  $('.board').on('click', '.add-card', function() {
+    $(this).siblings(".add-card-form-wrapper").removeClass("collapse")
+})
+
+$('.board').on('click', '.add-card-cancel', function() {
+  $(this).closest(".add-card-form-wrapper").addClass("collapse")
+});
+
+$(".add-card-save").on("click", function() {
+   var written = $(".add-card-form-wrapper .form-control").val();
+   var $this = $(this);
+   var currentContainer = $this.closest('.card-container')
+
+   var newComment = $(`
+    <div class="card">
+      <span class="card-more">
+        <span class="glyphicon glyphicon-align-left"></span>
+      </span>
+      <div class="card-body"> ${written} </div>
+    </div>
+   `)
+
+   currentContainer$.append(newComment)
+ });
+
+
+
+
+
+
+
+  });

749b256e3da2a333ab6680d9fb7c56d077681750
JingyW <wangj2@whitman.edu>
2017-06-08 09:35:18 -0700

    Finish part1 morning

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$.ajax({
+         url:"http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+         success: function(content) {
+           var count = content.length;
+           $("#count").append($('<pre>')).text(count);
+         }
+        })

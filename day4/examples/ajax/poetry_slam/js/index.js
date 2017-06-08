35f62a7e416e8850c9d843a11f8c67cea262b2c4
SHEN CHENHUI <sch88180733@163.com>
2017-06-08 15:21:38 -0700

    update day4 morning

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+	url:"http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+	success: function(resp){    
+		// $(".head").append($('<pre>').text(resp));
+		var wordNum = resp.split(" ").length;
+		$('#count').text(wordNum);
+	}
+})

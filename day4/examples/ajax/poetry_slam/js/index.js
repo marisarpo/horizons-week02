9d0988dc6cf361ba1db0fcf83c27b7237cb098f1
vanessacortes <vcortes@berkeley.edu>
2017-06-08 13:48:14 -0700

    day 4

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,12 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp) {
+    $("body").append($("<pre>").text(resp));
+    var poem = $("<pre>").text(resp);
+    var poemText = JSON.stringify(poem[0].innerText);
+    var count = poemText.split(" ").length;
+    $("#count").text(count);
+  }
+})

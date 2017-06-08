93cd6aed6011409aeb3ee98df53315e57198c213
Todd <allento@sas.upenn.edu>
2017-06-08 14:53:50 -0400

    day 4 app work

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,10 +1,18 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
 
+//$.ajax;
+
+
 $.ajax({
-	url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt'
-	//success: function(resp){
+	url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt',
+	success: function(resp){
     //console.log(resp);
-    //$('body').append($('<pre>').text(resp))
-	//}
+		var poem = resp;
+		console.log(poem);
+		var array = poem.split(" ");
+    $('body').append($('<pre>').text(resp));
+		var number = array.length;
+		$('#count').text(number);
+	}
 })
--- day4/twilio/twilio.js
+++ day4/twilio/twilio.js
@@ -3,9 +3,9 @@
 
 function TwilioApp() {
   // Part 0. Get Twilio credentials
-  this.accountId = "YOUR ACCOUNT ID HERE";
-  this.authToken = "YOUR AUTH TOKEN HERE";
-  this.fromNumber = "YOUR TWILIO NUMBER HERE";
+  this.accountId = "ACd06dbf8b6b15e16268636385508a88f3";
+  this.authToken = "0e50bcebb4bb99511ea0b78fe210b96a";
+  this.fromNumber = "15414184051";
 
   // Reference JQuery objects
   this.messageList = $(".message-list");
@@ -16,27 +16,72 @@ function TwilioApp() {
   // Set up the event handlers
   this.initialize();
 
-  console.log("TwilioApp is ready.");
+  //console.log("TwilioApp is ready.");
 }
 
 TwilioApp.prototype = {
   // Part 1. `initialize()` method
   initialize: function() {
     // YOUR CODE HERE
+    this.messageSendButton.on("click", function(event){
+      this.handleMessageSend(event);
+
+    }.bind(this))
   },
   // Part 2. `validateMessageField(textStr<String>)` method
   validateMessageField: function(textStr) {
     // YOUR CODE HERE
+    if($.trim(textStr).length === 0){
+      return false;
+    }
+    return true;
   },
   // Part 3. `validatePhoneField(phoneStr<String>)` method
   validatePhoneField: function(phoneStr) {
     // YOUR CODE HERE
+    var test = true;
+    if(phoneStr.length !== 10){
+      return false;
+    }
+    phoneStr.split("").forEach(function(item){
+      if(!Number.isInteger(parseInt(item))){
+        test = false;
+      }
+    })
+    return test;
   },
   // Part 4. `handleMessageSend(evt<Event>)` method
   handleMessageSend: function(event) {
     // YOUR CODE HERE
-    // REMOVE THE NEXT LINE, IT'S FOR TEST
-    this.displayMessage('9999999999', 'Testing testing!');
+    event.preventDefault();
+    var message = this.messageInputField.val();
+    var number = this.phoneInputField.val();
+
+    if(!this.validateMessageField(message)){
+      return false;
+    }
+    if(!this.validatePhoneField(number)){
+      return false;
+    }
+
+    $.ajax('https://api.twilio.com/2010-04-01/Accounts/' + this.accountId + '/SMS/Messages', {
+    success: function(x) {
+      this.displayMessage(this.fromNumber, this.messageInputField.val())
+    }.bind(this),
+    error: function(err){
+      alert("It's bad, mmmkay!");
+    },
+    method: 'POST',
+    data: {
+      From: this.fromNumber,
+      To: this.phoneInputField.val(),
+      Body: this.messageInputField.val()
+    },
+    headers: {
+      "Authorization": "Basic " + btoa(this.accountId + ":" + this.authToken)
+    }
+  });
+
   },
   displayMessage: function(sender, message) {
     var listElem = $('<li></li>').addClass('message');

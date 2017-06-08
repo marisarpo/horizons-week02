f2cdb4c56b2d2a812007f6ba301cb1bc1881e347
jatparam <jatharsan.param@gmail.com>
2017-06-08 17:26:25 -0400

    committing

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url: "http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+  success: function(resp) {
+    var arr = resp.split(" ")
+
+    $("#count").replaceWith(arr.length)
+  }
+})

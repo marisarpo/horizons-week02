44012c4318f759aa460bb361e946129e659f48a8
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-06-08 14:10:09 -0700

    Day 4 Half way

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,13 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+console.log("hello")
+
+var poemLength;
+
+$.ajax({
+	url: "https://horizons-json-cors.s3.amazonaws.com/poem.txt",
+	success: function(resp) {
+		poemLength = resp.split(" ").length;
+		$("#count").text(poemLength);
+	}
+})
--- day4/twilio/twilio.js
+++ day4/twilio/twilio.js
@@ -3,9 +3,9 @@
 
 function TwilioApp() {
   // Part 0. Get Twilio credentials
-  this.accountId = "YOUR ACCOUNT ID HERE";
-  this.authToken = "YOUR AUTH TOKEN HERE";
-  this.fromNumber = "YOUR TWILIO NUMBER HERE";
+  this.accountId = "ACd1448fb1345e6e2e045615bda02c80fc";
+  this.authToken = "3640a9c534f896a2ce1cb953f5fa4bc1";
+  this.fromNumber = "13213513661";
 
   // Reference JQuery objects
   this.messageList = $(".message-list");
@@ -23,18 +23,25 @@ TwilioApp.prototype = {
   // Part 1. `initialize()` method
   initialize: function() {
     // YOUR CODE HERE
+    var thisApp = this;
+    thisApp.messageSendButton.on("click", function(event) {
+      thisApp.handleMessageSend(event);
+    });
   },
   // Part 2. `validateMessageField(textStr<String>)` method
   validateMessageField: function(textStr) {
     // YOUR CODE HERE
+    return textStr.trim().length > 0;
   },
   // Part 3. `validatePhoneField(phoneStr<String>)` method
   validatePhoneField: function(phoneStr) {
     // YOUR CODE HERE
+    return !!(phoneStr.length == 10 && phoneStr.match(/[0-9]{10}/))
   },
   // Part 4. `handleMessageSend(evt<Event>)` method
   handleMessageSend: function(event) {
     // YOUR CODE HERE
+    event.preventDefault();
     // REMOVE THE NEXT LINE, IT'S FOR TEST
     this.displayMessage('9999999999', 'Testing testing!');
   },

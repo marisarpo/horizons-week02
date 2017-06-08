5a614dba53633f73291b8f6f5d0a241e18d11cee
Adam Ripley <aripley@wharton.upenn.edu>
2017-06-08 10:08:05 -0700

    june8

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url: 'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp) {
+    //console.log($('body').text());
+    var wordNum = resp.split(' ').length;
+    //console.log($(‘<pre>’).text(resp.split(' ').length));
+    $('#count').text(wordNum);
+  }
+})

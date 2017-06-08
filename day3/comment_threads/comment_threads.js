ec0bf1062b7b62938142117e89c7028d1fb312a6
asif521 <mbhatti2@illinois.edu>
2017-06-08 14:24:17 -0700

    commit

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,137 @@
 "use strict";
 
-// This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+//Click Handler
+function addComment() {
+  var userAuthor = prompt("Enter your name: ");
+  var userComment = prompt("Enter your comment: ");
+
+  var comment = $('<div>')
+    .addClass('comment');
+  var author = $('<div>')
+    .addClass('author')
+    .text(userAuthor + ' says:');
+  var message = $('<div>')
+    .addClass('message')
+    .text(userComment);
+  var controls = $('<div>')
+    .addClass('controls');
+
+  var buttonHide = $('<button>')
+    .addClass('hide-replies btn btn-default')
+    .text('Hide Replies');
+  var buttonShow = $('<button>')
+    .addClass('show-replies btn btn-default')
+    .text('Show Replies');
+  var reply = $('<button>')
+    .addClass('reply btn btn-default')
+    .text('Reply');
+
+  var replies = $('<div>')
+    .addClass('replies');
+
+  controls.append(buttonHide)
+    .append(buttonShow)
+    .append(reply);
+  comment.append(author)
+    .append(message)
+    .append(controls)
+    .append(replies);
+
+  return comment;
+}
+
+$('.post')
+  .on('click', function() {
+    var comment = addComment();
+    $('.comments')
+      .append(comment);
+
+  });
+
+// reply Handler
+$('.comments')
+  .on('click', '.reply', function() {
+    var comment = addComment();
+    $(this)
+      .parent()
+      .siblings('.replies')
+      .append(comment);
+
+  });
+
+// hide reply event handler
+$('.comments')
+  .on('click', '.hide-replies', function() {
+    $(this)
+      .parent()
+      .siblings('.replies')
+      .hide()
+
+  });
+
+// show reply event Handler
+$('.comments')
+  .on('click', '.show-replies', function() {
+    $(this)
+      .parent()
+      .siblings('.replies')
+      .show()
+
+  });
+
+
+// // This file contains JavaScript that will run on your page.
+// // Created DOM dynamically in JS
+//
+// // $('.post')
+// //   .on('click', function() {
+// //
+//     var userAuthor = prompt("Enter your name:");
+//     var userComment = prompt('Enter your comment:')
+//
+//     var comment = $('<div>')
+//       .addClass('comment');
+//     var author = $('<div>')
+//       .addClass('author')
+//       .text(userAuthor + 'says:');
+//     var message = $('<div>')
+//       .addClass('message')
+//       .text(userComment);
+//     var controls = $('<div>')
+//       .addClass('controls');
+//
+//     var buttonHide = $('<button>')
+//       .addClass('hide-replies btn btn-default')
+//       .text('Hide Replies');
+//     var buttonShow = $('<button>')
+//       .addClass('show-replies btn btn-default')
+//       .text('Show Replies');
+//     var buttonReply = $('<button>')
+//       .addClass('reply-replies btn btn-default')
+//       .text('Reply');
+//
+//     var replies = = $('<div>')
+//       .addClass('replies');
+//
+//     controls.append(buttonHide)
+//       .append(buttonShow)
+//       .append(buttonReply);
+//     comment.append(author)
+//       .append(message)
+//       .append(controls)
+//       .append(replies)
+//
+//     $('.comments')
+//       .append(comment);
+//
+//   });
+// .append(buttonReply);
+//     comment.append(author)
+//       .append(message)
+//       .append(controls)
+//       .append(replies)
+//
+//     $('.comments')
+//       .append(comment);
+//
+//   });
--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,13 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  
+  success: function(resp) {
+    var arr = resp.split(" ");
+    console.log(arr.length);
+
+    $('#count')
+      .replaceWith(arr.length)
+  }
+});

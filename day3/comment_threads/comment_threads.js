28169d8d72ad0b3b9ba2fdb743b4e0d7c4b8ada0
Oberon <ahijaouy@gatech.edu>
2017-06-08 09:08:54 -0700

    day 3

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -4,9 +4,53 @@
 // YOUR CODE HERE
 $(document).ready(function() {
 
+  //Set the correct Show/Hide status for initial comments
+  $(".comment").each(function(){
+
+    //hide all show reply buttons
+    $(this).children(".controls").children(".show-replies").hide()
+
+    //hide hide-reply button if there are no replies  
+    if(!$(this).children(".replies").children().length) {
+      $(this).children(".controls").children(".hide-replies").hide()
+      }
+  })
+
   //Post Comment Event Listener
   $(".post").on("click", function() {
-    var author = prompt("Enter your name");
+
+    //build DOM Element
+    var reply = getReply();
+
+    //Append to the Comments Div
+    $(".comments").append(reply);
+  });
+
+
+  //Reply to a Comment
+  $(".comments").on("click", ".comment .controls .reply", function() {
+    var reply = getReply();
+    var replies = $(this).parent().siblings(".replies").append(reply);
+  });
+
+  //Hide Comments
+  $(".comments").on("click", ".comment .controls .hide-replies", function() {
+    $(this).parent().siblings(".replies").hide();
+    $(this).hide();
+    $(this).siblings(".show-replies").show();
+  });
+
+  //Show Comments
+  $(".comments").on("click", ".comment .controls .show-replies", function() {
+    $(this).parent().siblings(".replies").show();
+    $(this).hide();
+    $(this).siblings(".hide-replies").show();
+  });
+
+});
+
+function getReply() {
+  var author = prompt("Enter your name");
     var comment = prompt("Enter your comment");
 
     //build DOM Element
@@ -22,10 +66,9 @@ $(document).ready(function() {
         <div class="replies"></div>
       </div>`);
 
-    //Append to the Comments Div
-    $(".comments").append(post);
-  });
+    return post;
+}
 
+function setHideShow() {
 
-  //
-});
+}
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,86 @@
 "use strict";
 
-// YOUR JAVASCRIPT CODE GOES HERE
+$(document).ready(function() {
+
+  //Show Add List Form
+  $(".add-list").on("click", function() {
+    $(this).siblings(".add-list-form-wrapper").removeClass("collapse");
+  });
+
+  //Close Add List Form
+  $(".add-list-cancel").on("click", function() {
+    $(this).parent().parent().addClass("collapse");
+  });
+
+  //Save Add List Form
+  $(".add-list-save").on("click", function() {
+    var title = $(this).siblings("input").val();
+
+    var node = $(`<div class="list-container">
+                  <div class="list">
+                    <div class="list-header">
+                      <span class="list-title">`+ title +`</span>
+                    </div>
+                    <div class="list-cards"></div>
+                    <div class="list-footer">
+                      <button class="add-card">Add a card...</button>
+                      <div class="collapse add-card-form-wrapper">
+                        <div class="well add-card-form">
+                          <input type="text" class="form-control" placeholder="Card title">
+                          <button type="button" class="btn btn-default add-card-save">
+                            Save
+                          </button>
+                          <button type="button" class="btn btn-default add-card-cancel">
+                            <span class="glyphicon glyphicon-remove"></span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>`);
+    $(".add-list-container").parent().before(node);
+    $(this).parent().parent().addClass("collapse");
+  });
+
+
+  //Show Add Card Form
+  $(".board").on("click", ".list-container .list .list-footer .add-card",function() {
+    $(this).siblings(".add-card-form-wrapper").removeClass("collapse");
+  });
+
+
+ //Close Add Card Form
+  $(".board").on("click", ".list-container .list .list-footer .add-card-cancel",function() {
+    $(this).parent().parent().addClass("collapse");
+  });
+
+  //Save Card
+  $(".board").on("click", ".list-container .list .list-footer .add-card-save",function() {
+    var title = $(this).siblings("input").val();
+    var node = $(`<div class="card">
+                    <span class="card-more">
+                      <span class="glyphicon glyphicon-align-left"></span>
+                    </span>
+                    <div class="card-body">`+title+`</div>
+                  </div>`);
+
+    $(this).parent().parent().addClass("collapse");
+    var listFooter = $(this).parent().parent().parent().siblings(".list-cards").append(node);
+  });
+
+  //Edit Card
+  $(".board").on("click", ".card", function() {
+    var cardBody = $(this).children(".card-body");
+    var modalText = $("#card-edit").find("textarea");
+    modalText.val(cardBody.text());
+    $(".card-edit-save").on("click", function() {
+      cardBody.text(modalText.val());
+      $("#card-edit").modal('hide');
+      $(this).off("click");
+    })
+    $("#card-edit").modal('toggle');
+
+  })
+  
+
+});

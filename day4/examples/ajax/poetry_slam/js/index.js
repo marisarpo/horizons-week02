dfdb08c84a7d381156995839810370b7f32ef8d9
danhuiZ <danhuizzz@gmail.com>
2017-06-08 14:03:50 -0700

    Week 2 day 4

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,12 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url:'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success:function(res) {
+    console.log(res);
+    $('body').append($('<pre>').text(res));
+    var str = JSON.stringify(res);
+    var wordCount = str.split(' ').length;
+    $('#count').text(wordCount);
+  }
+})

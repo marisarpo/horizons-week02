f28e222dd44d209e260ab232d2502d290f8c9ba4
Evan <Evanp1324@gmail.com>
2017-06-07 22:05:05 -0700

    day 3 final commit

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,39 @@
 "use strict";
 
 // This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+
+
+// Post a comment handler!
+$('.post').on('click', function(){
+  storeCommentInfo(".comments");
+
+});
+
+// Reply to a comment!
+$(".comments").on('click', '.reply', function() {
+   storeCommentInfo($(this).parent().parent().children(".replies"))
+ });
+
+// Hide comments
+$(".comments").on('click', '.hide-replies', function() {
+  $(this).parent().parent().children(".replies").hide();
+});
+
+// Show comments
+$(".comments").on('click', '.show-replies', function() {
+  $(this).parent().parent().children(".replies").show();
+});
+
+
+
+function storeCommentInfo(selector){
+  var nameInfo = prompt("What is your name?");
+  var commentInfo = prompt("Please write your comment")
+  var newCommentStr = "<div class=\"comment\"> <div class=\"author\">" + nameInfo +
+  "\" says:</div> <div class=\"message\">" + commentInfo
+  + "</div> <div class=\"controls\"> <button class=\"hide-replies btn btn-default\">"
+  + "Hide Replies</button><button class=\"show-replies btn btn-default\">Show Replies</"
+  + "button><button class=\"reply btn btn-default\">Reply</button></div><div class=\"replies\"></div</div>";
+
+  $(selector).append(newCommentStr);
+}
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,110 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+var htmlListStr;
+var htmlCardStr;
+var cardBeingEdited = null;
+
+// .add-list clicked handler
+$('.add-list').on('click', function(){
+  expandAddList();
+});
+
+// .add-list-cancel handler
+$('.add-list-cancel').on('click', function(){
+  colapseAddList();
+});
+
+// Handle adding the new list!
+$('.add-list-save').on('click', function(){
+  var saveName = $('.add-list-form-wrapper').find('.form-control').val();
+  htmlListStr = makeListHTMLStr(saveName);
+  $(this).closest('.list-container').before(htmlListStr);
+  colapseAddList();
+});
+
+// .add-card expand handler
+$('.board').on('click', '.add-card', function(){
+  $(this).siblings().removeClass('collapse');
+})
+
+// .add-card-cancel handler
+$('.board').on('click', '.add-card-cancel', function(){
+  $(this).parent().parent().addClass('collapse');
+
+})
+
+
+$('.board').on('click', '.add-card-save', function(){
+  var saveTitle = $(this).siblings('.form-control').val();
+  htmlCardStr = makeCardHTMLStr(saveTitle);
+  $(this).parent().parent().parent().siblings('.list-cards').append(htmlCardStr);
+  colapseAddCard();
+});
+
+$('.board').on('click', '.card', function(){
+  // Save the clicked card
+  cardBeingEdited = $(this);
+
+  // Get the card Text
+  var cardTxt = cardBeingEdited.children().siblings('.card-body').text();
+
+  // Set the text of the model body
+  $('#card-edit-body').val(cardTxt);
+  $('#card-edit').modal();
+
+  
+})
+
+
+
+
+
+function colapseAddCard(){
+  $('.add-card-form-wrapper').addClass('collapse');
+}
+
+function colapseAddList(){
+  $('.add-list-form-wrapper').addClass('collapse');
+}
+
+function expandAddList(){
+  $('.add-list-form-wrapper').removeClass('collapse');
+}
+
+function makeListHTMLStr(listTitle){
+return  `<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title"> `
+      + listTitle +
+      `</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+            Save
+          </button>
+          <button type="button" class="btn btn-default add-card-cancel">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+}
+
+function makeCardHTMLStr(cardTitle){
+  return ` <div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">` + cardTitle + `</div>
+</div>`;
+
+}

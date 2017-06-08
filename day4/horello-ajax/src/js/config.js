bd7e7e29e4ad31f680286342758d7ff68e0afccd
crestwood204 <andong@sas.upenn.edu>
2017-06-08 13:30:02 -0700

    update for week2 day 4

--- day4/horello-ajax/src/js/config.js
+++ day4/horello-ajax/src/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "YOUR API KEY HERE";
-var apiToken = "YOUR API TOKEN HERE";
+var apiKey = "567679872ed1b2e3e8e55702650ca6da";
+var apiToken = "5dda9370d2f8f73e0380f24891e06fb608bc29aa557ff023878dec3276e4ac21";
 var apiUrl = "https://api.trello.com/1";
 var boardId = "YOUR BOARD ID HERE";
--- day4/twilio/twilio.js
+++ day4/twilio/twilio.js
@@ -3,9 +3,9 @@
 
 function TwilioApp() {
   // Part 0. Get Twilio credentials
-  this.accountId = "YOUR ACCOUNT ID HERE";
-  this.authToken = "YOUR AUTH TOKEN HERE";
-  this.fromNumber = "YOUR TWILIO NUMBER HERE";
+  this.accountId = "AC7ae558ad6416477a41800381e5d6f39d";
+  this.authToken = "936a417af1fbdb21d8ae6fc24428a0a5";
+  this.fromNumber = "16093002375";
 
   // Reference JQuery objects
   this.messageList = $(".message-list");
@@ -23,20 +23,61 @@ TwilioApp.prototype = {
   // Part 1. `initialize()` method
   initialize: function() {
     // YOUR CODE HERE
+    var self = this;
+    this.messageSendButton.on('click', function (event) {
+      self.handleMessageSend('click');
+      event.preventDefault();
+    });
   },
   // Part 2. `validateMessageField(textStr<String>)` method
   validateMessageField: function(textStr) {
     // YOUR CODE HERE
+    if (textStr.trim() === '') {
+      return false;
+    } else {
+      return true;
+    }
   },
   // Part 3. `validatePhoneField(phoneStr<String>)` method
   validatePhoneField: function(phoneStr) {
     // YOUR CODE HERE
+    return $.isNumeric(phoneStr) && (phoneStr.length === 10);
   },
   // Part 4. `handleMessageSend(evt<Event>)` method
   handleMessageSend: function(event) {
     // YOUR CODE HERE
-    // REMOVE THE NEXT LINE, IT'S FOR TEST
-    this.displayMessage('9999999999', 'Testing testing!');
+    var self = this;
+    var msg_input = this.messageInputField.val();
+    var phone_input = this.phoneInputField.val();
+    var triggered = false;
+    if (!this.validatePhoneField(phone_input)) {
+      window.alert('invalid phone input');
+      triggered = true;
+    }
+    if (!this.validateMessageField(msg_input)) {
+      window.alert('invalid message input');
+      triggered = true;
+    }
+    if (!triggered) {
+      $.ajax('https://api.twilio.com/2010-04-01/Accounts/' + self.accountId + '/SMS/Messages', {
+        success: function(x) {
+          self.displayMessage(self.fromNumber, msg_input);
+          self.messageInputField.val('');
+        },
+        method: 'POST',
+        data: {
+          From: self.fromNumber,
+          To: parseInt(phone_input),
+          Body: msg_input
+        },
+        headers: {
+          "Authorization": "Basic " + btoa(self.accountId + ":" + self.authToken)
+        },
+        error: function (err) {
+          window.alert('an error has occured: ', err);
+        }
+      })
+    }
   },
   displayMessage: function(sender, message) {
     var listElem = $('<li></li>').addClass('message');

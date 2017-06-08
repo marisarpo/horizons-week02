574aa723c16a23cde90652353e3ca724e04a3a8d
Pam Needle <pneedle@colgate.edu>
2017-06-08 11:57:54 -0700

    completed indepedent work

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,13 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+//1184
+
+$.ajax({
+  url: 'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp){
+    var wordarray = resp.split(" ");
+    // console.log(resp);
+    var count = wordarray.length;
+    $('#count').text(count);
+  }
+})

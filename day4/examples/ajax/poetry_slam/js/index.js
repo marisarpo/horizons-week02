6d8289ff14362f648e230f11fad09e5397fcabab
Oberon <ahijaouy@gatech.edu>
2017-06-08 13:47:12 -0700

    video lectures

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,15 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$(document).ready(function() {
+
+  $.ajax({
+    url: "http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+    success: function(res) {
+      $("body").append($("<pre>").text(res));
+      $("#count").text(res.split(" ").length);
+    }
+  });
+
+
+});

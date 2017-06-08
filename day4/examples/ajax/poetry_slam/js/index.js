625dce610376f9e9d9f29659252a251824d9ff3a
Ben Hubsch <bah37@duke.edu>
2017-06-08 11:30:34 -0700

    Morning code-along

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,24 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$(document).ready(function() {
+
+
+
+$.ajax({
+    url: "http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+    success: function(resp) {
+        var ans = resp.split(" ").length
+        $("#count").text(ans)
+    }
+})
+
+
+
+
+
+
+
+
+
+})

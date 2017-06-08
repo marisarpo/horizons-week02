3ac7e702dd0ed5989ebbdfb889dac0bc54e24a40
Todd <allento@sas.upenn.edu>
2017-06-08 09:24:04 -0400

    day 4

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$.ajax({
+	url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt'
+	//success: function(resp){
+    //console.log(resp);
+    //$('body').append($('<pre>').text(resp))
+	//}
+})

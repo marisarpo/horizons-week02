13e6265480391016873f7abb01545e21d974f832
everuthingaud <setiadar@bc.edu>
2017-06-08 09:30:28 -0700

    help

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,54 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$(document).ready(function(){
+  //POST ON CLICK
+
+  $(".post").on("click", function(){
+    var au = prompt("Please enter your name", "name");
+    var cm = prompt("Enter comment", "comment");
+    var c = $('<div class="comment">');
+    c.append($('<div class="author">' + au +" says:</div>"));
+    c.append($('<div class="message">' + cm +"</div>"));
+    var ct = $('<div class="controls">');
+    ct.append($('<button class="hide-replies btn btn-default">Hide Replies</button>'));
+    ct.append($('<button class="show-replies btn btn-default">Show Replies</button>'));
+    ct.append($('<button class="reply btn btn-default">Reply</button>'));
+    c.append(ct);
+    c.append($('<div class="replies"></div>'));
+    $(".comments").append(c);
+  });
+
+  //$(".reply").on("click", function(){
+  $(".comments").on("click", ".reply", function() {
+    var au = prompt("Please enter your name", "name");
+    var cm = prompt("Enter comment", "comment");
+    var c = $('<div class="comment">');
+    c.append($('<div class="author">' + au +" says:</div>"));
+    c.append($('<div class="message">' + cm +"</div>"));
+    var ct = $('<div class="controls">');
+    ct.append($('<button class="hide-replies btn btn-default">Hide Replies</button>'));
+    ct.append($('<button class="show-replies btn btn-default">Show Replies</button>'));
+    ct.append($('<button class="reply btn btn-default">Reply</button>'));
+    c.append(ct);
+    c.append($('<div class="replies"></div>'));
+    var parComment = $(this).closest('.comment');
+    var rep = parComment.children('.replies');
+    rep.append(c);
+  });
+
+  $(".comments").on("click", ".hide-replies", function(){
+    var comm = $(this).closest('.comment');
+    var reps = comm.children('.replies');
+    reps.hide();
+  });
+
+  $(".comments").on("click", ".show-replies", function(){
+    var comm = $(this).closest('.comment');
+    var reps = comm.children('.replies');
+    reps.show();
+  });
+
+
+
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,87 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+$(document).ready(function (){
+  //add list gen
+  $(".add-list").on("click", function(){
+    $('.add-list-form-wrapper').removeClass('collapse');
+  });
+
+  //add list cancel
+  $(".add-list-cancel").on("click", function(){
+    $(".add-list-form-wrapper").addClass("collapse");
+  });
+
+  //add list save
+  $(".add-list-save").on("click", function(){
+    var inp = $(".add-list-form-wrapper input").val();
+    var str = `<div class="list-container">
+      <div class="list">
+        <div class="list-header">
+          <span class="list-title">` + inp + `</span>
+        </div>
+        <div class="list-cards"></div>
+          <div class="list-footer">
+            <button class="add-card">Add a card...</button>
+            <div class="collapse add-card-form-wrapper">
+              <div class="well add-card-form">
+                <input type="text" class="form-control" placeholder="Card title">
+                <button type="button" class="btn btn-default add-card-save">
+                  Save
+                </button>
+                <button type="button" class="btn btn-default add-card-cancel">
+                  <span class="glyphicon glyphicon-remove"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    $(this).closest('.list-container').before($(str));
+    $('.add-list-form-wrapper').addClass('collapse');
+  });
+
+  //add card
+  $(".board").on("click", ".add-card", function (){
+    var wrap = $(this).siblings(".add-card-form-wrapper");
+    wrap.removeClass('collapse');
+  });
+
+  //add card cancel
+  $(".board").on("click", ".add-card-cancel", function(){
+    var wrap = $(this).closest(".add-card-form-wrapper");
+    wrap.addClass('collapse');
+  });
+
+  //add card save
+  $(".board").on("click", ".add-card-save", function(){
+    var wrap = $(this).siblings("input");
+    var inp = wrap.val();
+    console.log(inp);
+    var str = `<div class="card">
+      <span class="card-more">
+        <span class="glyphicon glyphicon-align-left"></span>
+      </span>
+      <div class="card-body">` + inp + `</div>
+    </div>`;
+    $(this).closest(".list").find(".list-cards").append($(str));
+  });
+
+  var cardBeingEdited = null;
+
+  $(".board").on("click", ".card", function (){
+    cardBeingEdited = this;
+    var m = $("#card-edit");
+    m.modal();
+    var body = $(this).find(".card-body").text();
+    //console.log(body);
+    m.("#card-edit-body").val(body);
+
+
+  });
+
+
+
+
+
+});

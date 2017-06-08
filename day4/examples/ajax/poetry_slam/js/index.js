1a3f8765e6335b78fbc8cf7c00535b5bb0d85b4d
crestwood204 <andong@sas.upenn.edu>
2017-06-08 09:48:19 -0700

    day4

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,6 +1,16 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
 $(document).ready(function () {
-
-  
+  var count = 0;
+  $.ajax({
+    url: "https://horizons-json-cors.s3.amazonaws.com/poem.txt",
+    success: function (data) {
+      var text = $('pre').text();
+      console.log(text);
+      count = text.split(' ').length;
+      console.log('done');
+    }
+  });
+  console.log(count);
+  $('#count').val(count);
 })

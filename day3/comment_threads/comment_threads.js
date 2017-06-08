58ff5b4f21e75ffcaae4b1c8f0e10a14c561bdea
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-06-07 18:48:53 -0700

    Day 3 work

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,43 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$('.show-replies').hide()
+
+$('.post').on('click', function() {
+  var author = prompt('Enter an author name') || 'anonymous';
+  var comment = prompt('Enter your comment') || 'pigs are friends, not food';
+  var postComment = $(`<div class="comment"> <div class="author"> ${author} says:</div> <div class="message"> ${comment} </div> <div class="controls"> <button class="hide-replies btn btn-default">Hide Replies</button> <button class="show-replies btn btn-default">Show Replies</button> <button class="reply btn btn-default">Reply</button> </div> <div class="replies"></div> </div>`);
+  $('.container .col-xs-10 .comments').append(postComment);
+})
+//not sure why .comments work but .comment does not work -- ask question
+$('.comments').on('click', '.reply', function() {
+  var author = prompt('Enter an author name') || 'anonymous';
+  var comment = prompt('Enter your comment') || 'pigs are friends, not food';
+  var postComment = $(`<div class="comment"> <div class="author"> ${author} says:</div> <div class="message"> ${comment} </div> <div class="controls"> <button class="hide-replies btn btn-default">Hide Replies</button> <button class="show-replies btn btn-default">Show Replies</button> <button class="reply btn btn-default">Reply</button> </div> <div class="replies"></div> </div>`);
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(postComment);
+})
+
+$('.comments').on('click', '.hide-replies', function() {
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  commentDiv.children('.controls').children('.show-replies').toggle()
+  commentDiv.children('.controls').children('.hide-replies').toggle()
+  var numReplies = repliesDiv.find('.comment').length;
+  commentDiv.append(`<h1 class="numReplies">${numReplies}</h1>`);
+  repliesDiv.hide();
+})
+
+$('.comments').on('click', '.show-replies', function() {
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  commentDiv.children('.controls').children('.show-replies').toggle()
+  commentDiv.children('.controls').children('.hide-replies').toggle()
+  var numReplies = repliesDiv.find('.comment').length;
+  $('.numReplies').hide()
+  repliesDiv.show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,96 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+var cardBeingEdited = null;
+
+$(window).on('keydown',function(event){
+  var x= String.fromCharCode(event.which)
+  if(x==='C'){
+    $('.card:hover').remove();
+  }
+})
+
+$('.board').on('click', '.card', function() {
+  cardBeingEdited = $(this);
+  $("#card-edit").modal()
+  var cardVal = cardBeingEdited.find('.card-body').text()
+  $("#card-edit-body").val(cardVal)
+})
+
+$('.card-edit-save').on('click', function() {
+  var text = $("#card-edit-body").val();
+  cardBeingEdited.find('.card-body').text(function() {
+    return (text)
+  })
+ })
+
+$('.list-cards').sortable({
+  reverse: false,
+  items: '.card',
+  connectWith: ".list-cards"
+});
+
+
+
+
+$('.add-list').on('click', function() {
+  $('.add-list-form-wrapper').removeClass('collapse');
+})
+
+$('.add-list-cancel').on('click', function(){
+  $('.add-list-form-wrapper').addClass('collapse');
+})
+
+$('.add-list-save').on('click', function() {
+  var title = $('.add-list-form-wrapper input').val();
+  var listAdd = (`<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title">${title}</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+            Save
+          </button>
+          <button type="button" class="btn btn-default add-card-cancel">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`);
+$('.add-list-form-wrapper').closest('.list-container').before(listAdd)
+$('.add-list-form-wrapper').addClass('collapse');
+})
+
+$('.board').on('click', '.add-card', function() {
+  var $this = $(this);
+  var addcardDiv = $this.siblings('.add-card-form-wrapper')
+  addcardDiv.removeClass('collapse');
+})
+
+$('.board').on('click','.add-card-cancel', function() {
+  var $this = $(this);
+  var addcardDiv = $this.closest('.add-card-form-wrapper')
+  addcardDiv.addClass('collapse');
+})
+
+$('.board').on('click','.add-card-save', function() {
+  var $this = $(this);
+  var title = $this.siblings('input').val()
+  var cardAdd = (`<div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">${title}</div>
+</div>`);
+var listCards = $this.closest('.list-footer').siblings('.list-cards');
+listCards.append(cardAdd)
+
+})

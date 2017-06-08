d290ba1230b04b3740deb0f341fef9791a9687f9
git push -u origin <lmccafferty1@babson.edu>
2017-06-08 13:36:28 -0700

    Save

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,50 @@
 "use strict";
+$('#posted').on('click', function() {
+  var authorName = prompt("author");
+  var comm = prompt("comment");
+  var newElement = $(`<div class="comment">
+  <div class="author">${authorName} says:</div>
+  <div class="message">${comm}</div>
+  <div class="controls">
+  <button class="hide-replies btn btn-default">Hide Replies</button>
+  <button class="show-replies btn btn-default">Show Replies</button>
+  <button class="reply btn btn-default">Reply</button>
+  </div>
+  <div class="replies"></div>
+  </div>`)
+  $('.comments').append(newElement);
+})
 
-// This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+$('.comments').on('click', '.reply', function () {
+  var authorName = prompt("author");
+  var comment = prompt("comment");
+  var newElement = $(`<div class="comment">
+  <div class="author">${authorName} says:</div>
+  <div class="message">${comment}</div>
+  <div class="controls">
+  <button class="hide-replies btn btn-default">Hide Replies</button>
+  <button class="show-replies btn btn-default">Show Replies</button>
+  <button class="reply btn btn-default">Reply</button>
+  </div>
+  <div class="replies"></div>
+  </div>`)
+  // `this` points to the current `.reply` button that was clicked
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(newElement);
+})
+
+$('.comments').on('click', '.hide-replies', function () {
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.hide();
+})
+
+$('.comments').on('click', '.show-replies', function () {
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,16 @@
 "use strict";
-
 // YOUR JAVASCRIPT CODE GOES HERE
+$(document).ready(function() {
+$('.add-list').on('click', function() {
+  $('.add-list-form-wrapper').removeClass('collapse')
+})
+$('.add-list-cancel').on('click', function() {
+  $('.add-list-form-wrapper').addClass('collapse')
+})
+})
+
+$('.add-list-save').on('click', function() {
+  var title = $('.add-list-form-wrapper .form-control').val();
+  var newCard = $('<div class="list-container"><div class="list"><div class="list-header"><span class="list-title">' + title + '</span></div><div class="list-cards"></div><div class="list-footer"><button class="add-card">Add a card...</button><div class="collapse add-card-form-wrapper"><div class="well add-card-form"><input type="text" class="form-control" placeholder="Card title"><button type="button" class="btn btn-default add-card-save">Save</button><button type="button" class="btn btn-default add-card-cancel"><span class="glyphicon glyphicon-remove"></span></button></div></div></div></div></div>');
+  $('.add-list-form-wrapper').parent().parent().before(newCard);
+});

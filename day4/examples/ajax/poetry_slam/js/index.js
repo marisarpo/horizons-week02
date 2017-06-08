3c466ae72fbecbab2a4048aa33ea2364c6e24677
Henry Z <henryzzuo@gmail.com>
2017-06-08 15:21:12 -0700

    week2 day4 afternoon update

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$.ajax({
+  url: 'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp){
+    var count = resp.split(' ').length;
+    $('#count').text(count);
+    $('.head').append($('<p>').text(resp));
+  }
+})

f1154fa40549196850c05ea18b3311efca663f7f
jackkimball13 <john.m.kimball@uconn.edu>
2017-06-08 10:14:33 -0700

    day4

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE
+console.log($.ajax)
+$.ajax({
+  url: 'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success: function(resp) {
+    resp = resp.replace(/ +/g, ' ')
+    console.log( _.countBy(resp));
+  }
+})
+
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt

adfdd4134c8c8c1893ea9acaa1b44264ed545552
marinabacha <marinabacha@nyu.edu>
2017-06-08 14:05:52 -0700

    changes2 day3 week2

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,17 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+$.ajax({
+  url:'http://horizons-json-cors.s3.amazonaws.com/poem.txt',
+  success:function(resp){
+    var myText = resp;
+    $('body').append($('<pre>').text(myText))
+    var words = myText.split(" ");
+    var count = words.length;
+    console.log(count);
+    $('#count').text(count);
+  }
+
+
+
+
+})

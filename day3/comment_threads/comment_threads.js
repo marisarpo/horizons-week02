ad5ddd60e28456208b543de5f3fb6b257bc50a9e
maxwell3739 <maxwell@uvic.ca>
2017-06-08 09:22:50 -0700

    add day 1-2 week02

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,38 @@
 "use strict";
 
 // This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+$('.post').on('click', function() {
+var userName = prompt('Enter your name')
+var userEntry = prompt('Enter your comment')
+var postedElement = $('<div class="comment"><div class="author"> "' + userName + '" says:</div><div class="message">' + userEntry + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+$('.comments').append(postedElement)
+})
+
+
+
+$('.comments').on('click', '.reply',
+    function() {
+  var userName = prompt('Enter your name')
+  var userEntry = prompt('Enter your comment')
+  var postedElement = $('<div class="comment"><div class="author"> "' + userName + '" says:</div><div class="message">' + userEntry + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>')
+  var $this = $(this);
+   var commentDiv = $this.closest('.comment');
+   var repliesDiv = commentDiv.children('.replies')
+   repliesDiv.append(postedElement);
+  })
+
+$('.comments').on('click', '.hide-replies',
+function() {
+var $this = $(this);
+var commentDiv = $this.closest('.comment');
+var repliesDiv = commentDiv.children('.replies')
+$(repliesDiv).hide();
+})
+
+$('.comments').on('click', '.show-replies',
+function() {
+var $this = $(this);
+var commentDiv = $this.closest('.comment');
+var repliesDiv = commentDiv.children('.replies')
+$(repliesDiv).show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,137 @@
 "use strict";
 
-// YOUR JAVASCRIPT CODE GOES HERE
+var cardBeingEdited = null
+
+var backgrounds = ["rgb(0,121,191)", "Cyan", "DarkSeaGreen", "LightSalmon", "url('img/bogdan-dada-156739.jpg')", "url('img/koushik-c-141142.jpg')", "url('img/Meeting-2340_CLIPCHAMP_keep.mp4')"]
+var index = 1
+
+$('.add-list').on('click', function() {
+    $('.add-list-form-wrapper').removeClass('collapse');
+})
+
+$('.add-list-cancel').on('click', function() {
+    $('.add-list-form-wrapper').addClass('collapse');
+})
+
+
+$('.add-list-save').on('click', function() {
+  var title = $('.add-list-form-wrapper').find('input').val()
+  var newElement = $(`<div class="list-container">
+    <div class="list">
+      <div class="list-header">
+        <span class="list-title">` + title + `</span>
+      </div>
+      <div class="list-cards"></div>
+      <div class="list-footer">
+        <button class="add-card">Add a card...</button>
+        <div class="collapse add-card-form-wrapper">
+          <div class="well add-card-form">
+            <input type="text" class="form-control" placeholder="Card title">
+            <button type="button" class="btn btn-default add-card-save">
+              Save
+            </button>
+            <button type="button" class="btn btn-default add-card-cancel">
+              <span class="glyphicon glyphicon-remove"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`);
+  $(this).closest('.list-container').before(newElement)
+})
+
+$('.add-list').on('click', function() {
+    $('.add-list-form-wrapper').removeClass('collapse');
+})
+
+
+
+
+
+$('.board').on('click', '.add-card', function() {
+  var $this = $(this);
+  var fotterDiv = $this.closest('.list-footer');
+  var wrapperDiv = fotterDiv.children('.add-card-form-wrapper')
+    wrapperDiv.removeClass('collapse');
+})
+
+$('.board').on('click', '.add-card-cancel', function() {
+    var $this = $(this);
+
+
+
+
+    var formWrap = $this.parent().parent()
+    formWrap.addClass('collapse');
+  })
+
+
+  $('.board').on('click', '.add-card-save', function() {
+    var $this = $(this);
+    var formWrap = $this.parent().parent();
+    var title = formWrap.find('input').val();
+
+    var newCard = `<div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">`+ title +`</div>
+</div>`
+var listFooter = $this.closest('.list-footer');
+var listCard = listFooter.siblings(".list-cards")
+listCard.append(newCard);
+var formWrapper = $this.closest('.add-card-form-wrapper');
+formWrapper.addClass('collapse')
+  })
+
+$('.board').on('click', '.card', function() {
+  cardBeingEdited = $(this);
+  var cardContent = cardBeingEdited.find('.card-body').text()
+  $('#card-edit-body').val(cardContent)
+  $("#card-edit").modal()
+// var userInput = $("#card-edit").closest('modal-body').val()
+    })
+
+
+$('body').on('click', '.card-edit-save', function() {
+  cardBeingEdited.find('.card-body').text($('#card-edit-body').val())
+  $('#card-edit').modal('toggle')
+        })
+
+$('.list-cards').sortable ({
+        connectWith: ('.list-cards')// Configuration parameters here
+    });
+
+$(document).on('keydown', function(/*event the happens*/) {
+var charCode = event.keyCode
+var letter = String.fromCharCode(charCode)
+
+
+if (letter === 'C') {
+  console.log("You pressed C")
+var onCard = $('.card:hover')
+onCard.remove()
+
+  }
+});
+
+$(document).on('click', '.header-logo', function() {
+  if(index>backgrounds.length-1) {index = 0}
+  if (index == 4 || index == 5 ) {
+    $('.board').css('background-image', backgrounds[index])
+    $('.board').css('background-size', '100%')
+  }
+  else if (index>5 ) {
+
+$(".board").before(`<div class="container">
+              <video class="vid" autoplay muted loop>
+                    <source src="img/Meeting-2340_CLIPCHAMP_keep.mp4">
+              </video>
+            </div>`)
+
+  } else {
+    $('.board').css('background', backgrounds[index])
+  }
+  index++
+})

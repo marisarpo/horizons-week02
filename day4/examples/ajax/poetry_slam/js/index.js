c1a39a3ac6649421fa45fb2f863615fedc604935
kritpetra <krit.petra@gmail.com>
2017-06-08 11:21:02 -0700

    AJAX examples

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,14 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+
+$(function() {
+
+  $.ajax({
+    url: "https://horizons-json-cors.s3.amazonaws.com/poem.txt",
+    success: function(resp) {
+      var numWords = resp.split(" ").length;
+      $('#count').text(numWords);
+    }
+  });
+
+});

6a8311ae229d9569e1a7a35181df4fbc970597de
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-06-08 09:25:15 -0700

    Day 3 Done

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,15 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$(".post").on("click", function() {
+	// var author = window.prompt("Enter name");
+	// var message = window.prompt("Enter comment");
+
+	var newComment = $('<div class="comment">')
+	newComment.append('<div class="author">')
+
+	$(".comments").append(newComment)
+});
+
+
+

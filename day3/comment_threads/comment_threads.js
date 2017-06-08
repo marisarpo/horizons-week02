a96e7f1d67693e2c2e867de51d36c8c9f2e15c8f
Alessandro Latif <alessandro@latif-khan.com>
2017-06-08 09:22:41 -0700

    week 2 thursday

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,52 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+$('document').ready(function(){
+  $('.show-replies').hide();
+})
+
+$('.post').on('click',function() {
+  var author = prompt('Enter your name');
+  var comment = prompt('Enter your comment');
+  if(author && comment){
+    $('.comments').append(input(author, comment));
+  }
+});
+
+
+function input(author, comment) {
+  var x = $('<div class="comment"></div>');
+  x.append($('<div class="author">').text('"' + author + '" says:'));
+  x.append($('<div class="message">').text(comment));
+  var controls = $('<div class="controls"></div>');
+  controls.append($('<button class="hide-replies btn btn-default">Hide Replies</button>'));
+  controls.append($('<button class="show-replies btn btn-default">Show Replies</button>'));
+  controls.append($('<button class="reply btn btn-default">Reply</button>'));
+  x.append(controls);
+  x.append($('<div class="replies"></div>'));
+  return x;
+}
+
+$('.comments').on('click', '.reply', function() {
+  var author = prompt('Enter your name');
+  var comment = prompt('Enter your comment');
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(input(author, comment));
+});
+
+$('.comments').on('click', '.hide-replies', function(){
+  var $this = $(this);
+  $this.closest('.comment').children('.replies').hide();
+  $this.hide();
+  $this.closest('.comment').find('.show-replies').show();
+})
+
+$('.comments').on('click', '.show-replies', function(){
+  var $this = $(this);
+  $this.closest('.comment').children('.replies').show();
+  $this.hide();
+  $this.closest('.comment').children('.controls').children('.hide-replies').show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,109 @@
 "use strict";
 
-// YOUR JAVASCRIPT CODE GOES HERE
+
+
+var cardBeingEdited = null;
+var x = null;
+
+
+$('.board').on('click', '.add-list', function(){
+  $('.add-list-form-wrapper').removeClass('collapse');
+})
+
+$('.board').on('click', '.add-list-cancel', function(){
+  $('.add-list-form-wrapper').addClass('collapse');
+})
+
+
+
+$('.board').on('click', '.add-list-save', function(){
+
+  var newContainer = `<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title">${$('.add-list-form-wrapper input').val()}</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+            Save
+          </button>
+          <button type="button" class="btn btn-default add-card-cancel">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+
+  newContainer = $(newContainer);
+  var x = $(this).closest('.list-container');
+  x.before(newContainer);
+})
+
+
+$('.board').on('click', '.add-card', function(){
+  $(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+})
+
+$('.board').on('click', '.add-card-cancel', function(){
+  $(this).parent().parent('.add-card-form-wrapper').addClass('collapse');
+})
+
+
+$('.board').on('click', '.add-card-save', function(){
+  var content = $(this).siblings('input').val()
+  var newCard = `<div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">${content}</div>
+</div>`
+
+  newCard = $(newCard);
+
+  var y = $(this).closest('.list-container');
+  y.find('.list-cards').append(newCard);
+})
+
+
+$('.board').on('click', '.card', function(){
+  cardBeingEdited = $(this);
+  $('#card-edit').modal();
+})
+
+$('.card-edit-save').on('click', function(){
+  var x = $('#card-edit-body').val();
+  cardBeingEdited.find('.card-body').text(x);
+  cardBeingEdited = null;
+  $('#card-edit').modal('hide');
+})
+
+
+$( ".list-cards" ).sortable({
+  connectWith: ".list-cards"
+}).disableSelection();
+
+$(document).on('keydown', function(event){
+  if (event.keyCode === 67 ){
+    $('.card:hover').remove();
+  }
+})
+
+
+function changeColor() {
+  var color_sets = ["#FF5733", "#FDFF33", "#4BFF33", "#33FFCC", "#334FFF", "#FF33F6"];
+  var color_sets_list = ["#794B77", "#4D4B79", "#4B795F", "#75794B", "#79504B"];
+  var color_sets_card = ["white", "blue", "red", "green", "brown"];
+  $('body').css('background-color', color_sets[Math.floor(Math.random() * color_sets.length)]);
+  $('.list').css('background', color_sets_list[Math.floor(Math.random() * color_sets_list.length)]);
+  $('.card').css('background', color_sets_card[Math.floor(Math.random() * color_sets_card.length)])
+}
+$('.header-logo-default').on('click', function() {
+  changeColor();
+})

51afaf046b80edf60313b4cd27882e067dd396bd
floresfxf <fflores@colgate.edu>
2017-06-09 14:37:17 -0700

    register and login

--- day5/facebook/js/script.js
+++ day5/facebook/js/script.js
@@ -12,14 +12,20 @@ $('.submit').on('click',function(){
   var $inputs = $('#login_form :input');
   var user = $inputs[0].value
   var password = $inputs[1].value
+  function clear(){
+       $('#input_form input').each(function () {
+         $(this).val("");
+      });
+          $('#input_form input').first().focus();
+  };
   $.ajax(apiUrl + '/users/login', {
       method: 'POST',
       success: function(data) {
         // data will be the response data that is
         // returned by the endpoint. use this to
         // access the token for future authorization.
-        console.log('hey');
         localStorage.setItem('token', data.response.token)
+        clear();
         $('.login-container').hide();
         // data.response.token will give you access
         // to the AUTH_TOKEN
@@ -27,10 +33,56 @@ $('.submit').on('click',function(){
       data: {
         email: user,
         password: password
+      },
+      error: function(err){
+        alert('ERROR',err);
       }
     });
+})
+$('#submit_reg').on('click',function(){
+  var $inputs = $('#registration_form :input');
+  var fname = $inputs[0].value
+  var lname = $inputs[1].value
+  var newUser = $inputs[2].value
+  var newPass = $inputs[3].value
+  function clear(){
+       $('#registration_form input').each(function () {
+         $(this).val("");
+      });
+          $('#registration_form input').first().focus();
+  };
+
+
+  $.ajax(apiUrl + '/users/register', {
+      method: 'POST',
+      data:{
+        fname: fname,
+        lname: lname,
+        email: newUser,
+        password: newPass
+      },
+      success: function(data) {
+        // data will be the response data that is
+        // returned by the endpoint. use this to
+        // access the token for future authorization.
+        $('.registration-container').hide();
+        clear();
+        $('.login-container').show();
+        // data.response.token will give you access
+        // to the AUTH_TOKEN
+      },
+      error: function(err){
+        alert('ERROR',err);
+        console.log(err);
+      }
+    });
+
 })
 $('#register_button').on('click', function(){
   $('.login-container').hide();
   $('.registration-container').show();
+})
+$('#login_btn').on('click', function(){
+  $('.login-container').show();
+  $('.registration-container').hide();
 })

04254801d412b3ad2ae9ab46d6d65223a12783d0
git status <xiaoxie@wharton.upenn.edu>
2017-06-09 13:19:51 -0700

    june 8 finished

--- day4/horello-ajax/src/js/config.js
+++ day4/horello-ajax/src/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "YOUR API KEY HERE";
-var apiToken = "YOUR API TOKEN HERE";
+var apiKey = "a796f25c78dd110193902aec5d1f0b02";
+var apiToken = "b93dcb4ce228799b5340d68ba0315839a0d5d2f5770ec6abf60abf82321c4acd";
 var apiUrl = "https://api.trello.com/1";
-var boardId = "YOUR BOARD ID HERE";
+var boardId = "5802b6341e37be0da4ad4f5d";
--- day4/twilio/twilio.js
+++ day4/twilio/twilio.js
@@ -3,9 +3,9 @@
 
 function TwilioApp() {
   // Part 0. Get Twilio credentials
-  this.accountId = 'AC1a73e4734c0a3b9a71a635c354c6e862';
-  this.authToken = '94836de818fe210f0b3f4b68f0e97dea';
-  this.fromNumber = +3524159792;
+  this.accountId = 'AC72981249d1a5337bf543bf5b7f0ca54d';
+  this.authToken = '998a8d315304d5006556b8ffad38d64e';
+  this.fromNumber = +3523534528;
 
   // Reference JQuery objects
   this.messageList = $(".message-list");
@@ -23,21 +23,53 @@ TwilioApp.prototype = {
   // Part 1. `initialize()` method
   initialize: function() {
     // YOUR CODE HERE
+    this.messageSendButton.on('click', this.handleMessageSend.bind(this))
   },
   // Part 2. `validateMessageField(textStr<String>)` method
   validateMessageField: function(textStr) {
     // YOUR CODE HERE
+    return textStr.trim().length > 0;
   },
   // Part 3. `validatePhoneField(phoneStr<String>)` method
   validatePhoneField: function(phoneStr) {
     // YOUR CODE HERE
+    var arr = phoneStr.split("");
+    var valid = arr.length === 10;
+
+    for (var i = 0; i < arr.length; i++) {
+      if(isNaN(parseInt(arr[i], 10))){
+        valid = false;
+      }
+    }
+    return valid;
   },
   // Part 4. `handleMessageSend(evt<Event>)` method
   handleMessageSend: function(event) {
     // YOUR CODE HERE
+    var myMessage = this.messageInputField.val();
+    var myPhoneNum = this.phoneInputField.val();
+    var self = this;
+    if(self.validateMessageField(myMessage) && self.validatePhoneField(myPhoneNum)){
+      $.ajax('https://api.twilio.com/2010-04-01/Accounts/' + self.accountId + '/SMS/Messages', {
+        success: function(x) {
+          self.displayMessage(myPhoneNum, myMessage);
+        },
+        method: 'POST',
+        data: {
+          From: self.fromNumber,
+          To: myPhoneNum,
+          Body: myMessage
+        },
+        headers: {
+          "Authorization": "Basic " + btoa(self.accountId + ":" + self.authToken)
+        }
+      });
+    }
+    event.preventDefault();
     // REMOVE THE NEXT LINE, IT'S FOR TEST
-    this.displayMessage('9999999999', 'Testing testing!');
+    // this.displayMessage('9999999999', 'Testing testing!');
   },
+
   displayMessage: function(sender, message) {
     var listElem = $('<li></li>').addClass('message');
     var senderElem = $('<span></span>').addClass('sender').text(sender);

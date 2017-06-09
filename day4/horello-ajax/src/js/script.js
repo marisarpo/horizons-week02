e28beaa184dcfa406230a258b400f3befd48e554
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-09 13:08:00 -0700

    done

--- day4/horello-ajax/src/js/script.js
+++ day4/horello-ajax/src/js/script.js
@@ -1,30 +1,33 @@
 // This is where you will write all your CODE
 // for the Horello AJAX (DAY 4) exercise.
 
+// BONUS 1
+$.ajaxSetup({
+  data: {
+    key: apiKey,
+    token: apiToken
+  }
+});
+
+// BONUS 2
+setInterval(render, 30000);
+
+
 $(document).ready(function() {
   setEventListeners();
   render();
-
-  // BONUS
-  console.log("hello")
-  $.ajaxSetup({
-    data: {
-      key: apiKey,
-      token: apiToken
-    }
-  });
-
 });
 
 function createList(listName) {
   // YOUR CODE HERE
-  $.ajax('https://api.Trello.com/1/lists', {
+  $.ajax({
+    url: 'https://api.Trello.com/1/lists',
     method: "POST",
     data: {
-      idBoard: boardId,
-      key: apiKey,
-      token: apiToken,
-      name: listName
+      // key: apiKey,
+      // token: apiToken,
+      name: listName,
+      idBoard: boardId
     },
     success: function(data){ render(); },
     error: function(err){ console.log(err); }
@@ -32,13 +35,14 @@ function createList(listName) {
 }
 
 function createCard(name, listId) {
-  $.ajax('https://api.Trello.com/1/cards', {
+  $.ajax({
+    url: 'https://api.Trello.com/1/cards',
     method: "POST",
     data: {
+      // key: apiKey,
+      // token: apiToken,
       name: name,
-      idList: listId,
-      key: apiKey,
-      token: apiToken
+      idList: listId
     },
     success: function(data){ render(); },
     error: function(err){ console.log(err); }
@@ -46,30 +50,45 @@ function createCard(name, listId) {
 }
 
 function updateCard(title, desc, cardId) {
-  $.ajax('https://api.Trello.com/1/cards/'+cardId, {
+  $.ajax({
+    url: 'https://api.Trello.com/1/cards/'+cardId,
     method: "PUT",
     data: {
+      // key: apiKey,
+      // token: apiToken,
       name: title,
-      desc: desc,
-      key: apiKey,
-      token: apiToken
+      desc: desc
     },
     success: function(data){ render(); },
-    error: function(err){ console.log("Error", err); }
-  });
+    // if the update fails, retain the current description
+    error: function(err){
+      console.log("Error", err);
+      // put the desc and title into the card
 
+      // change the modal
+      $('#'+cardId).data('cardDesc', desc);
+      $('#'+cardId).data('cardName', title);
+
+      // change the body
+      $("#"+cardId+" .card-body").text(title);
+
+    }
+  });
 }
 
 function render() {
-  $.ajax('https://api.Trello.com/1/boards/'+ boardId, {
+  $.ajax({
+    url: 'https://api.Trello.com/1/boards/'+boardId,
     data: {
-      key: apiKey,
-      token: apiToken,
+      // key: apiKey,
+      // token: apiToken,
       cards: 'all',
       lists: 'all'
     },
-    success: function(data) { renderBoard(data) }
+    success: function(data) { renderBoard(data); },
+    error: function(err) { console.log("err", err); }
   });
+
 }
 
 function renderBoard(board) {
@@ -97,7 +116,6 @@ function renderBoard(board) {
 }
 
 function renderList(list) {
-  // YOUR CODE HERE
   var text = `<div class="list-container">
   <div class="list" data-list-id=`+list.id+` id=`+list.id+`>
   <div class="list-header">

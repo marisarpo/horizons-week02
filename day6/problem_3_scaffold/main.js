c8e93c6ee20f2d8b389516dc9d4a6584b6c92e90
JingyW <wangj2@whitman.edu>
2017-06-11 20:31:30 -0700

    Got the keep rolling

--- day6/problem_3_scaffold/main.js
+++ day6/problem_3_scaffold/main.js
@@ -1,25 +1,39 @@
 var body = document.body,
     html = document.documentElement;
+var nameCount = 0;
 
 var height = Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+addMore();
+function addMore() {
+ var allNewNames = "";
+ var nameArr = [];
+ for (var i=0; i<50; i++){
+   $.ajax('https://randomuser.me/api/', {
+     success: function(data) {
+       var newName = "";
+       newName += data.results[0].name.first+" "+data.results[0].name.last;
+       var newNameDiv = $(`<div class="tile">
+                       		${newName}
+                       	</div>`);
+       nameCount++;
+       $('body').append(newNameDiv)
+       $('.countNumber').text(nameCount);
+       if (nameCount%50 === 0) {
+         $('.staStop').text("stop");
+         setTimeout(function() {
+           $('.staStop').text("");
+         }, 2000);
+       }
+     }
+   })
+ }
+}
+
 $(window).scroll(function() {
-  console.log($(window).scrollTop())
-  console.log($(window).height())
-   if($(window).scrollTop() + height === $(document).height()-1) {
-       addMore();
+   if(parseInt($(window).scrollTop() + height) === $(document).height()-1) {
+     console.log("got")
+     $('.staStop').text("start");
+     addMore();
    }
 });
-
-function addMore() {
-  for (var i=0; i<50; i++){
-    $.ajax('https://randomuser.me/api/', {
-      success: function(data) {
-        console.log(data)
-      }
-    })
-    var newNameDiv = `<div class="tile">
-                    		${newName}
-                    	</div>`
-  }
-}

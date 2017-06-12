8d451650b973601b1b88d4c745255fd22b018db3
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-06-11 18:28:15 -0700

    day-3 parallax bonus

--- day5/facebook/js/script.js
+++ day5/facebook/js/script.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
   var isLoginMode = true;
   var userId;
   var userToken;
+  var firstName;
+  var lastName;
 
   $(".registerBt").on('click', function(event){
     event.preventDefault();
@@ -16,8 +18,8 @@ $(document).ready(function(){
     } else{
       var newEmail = $(".emailFld").val();
       var newPassword = $(".passwordFd").val();
-      var newFirstName = $(".firstNameFld").val();
-      var newLastName = $(".lastNameFld").val();
+      var firstName = $(".firstNameFld").val();
+      var lastName = $(".lastNameFld").val();
       $.ajax({
         url: baseUrl + "users/register",
         method: 'POST',
@@ -25,8 +27,8 @@ $(document).ready(function(){
           toLoginPage();
         },
         data: {
-          fname: newFirstName,
-          lname: newLastName,
+          fname: firstName,
+          lname: lastName,
           email: newEmail,
           password: newPassword
         },
@@ -38,6 +40,24 @@ $(document).ready(function(){
 
 
 
+})
+
+$("post-btn").on('click', function(event){
+  console.log('button pressed')
+  var postContent = $(this).val();
+  event.preventDefault();
+  $.ajax({
+    url: baseUrl + "/posts",
+    method: "POST",
+    data: {
+      token: userToken,
+      content: postContent
+    },
+    success: function(){
+      console.log('worked')
+      refreshData();
+    }
+  })
 })
 
 $(".loginBt").on('click', function(event){
@@ -100,7 +120,7 @@ function toMainPage(){
   $('.login-container').remove();
   $('.container').append('<div class="view-container"></div>')
   refreshData()
-  setInterval(refreshData, 1000);
+  setInterval(refreshData, 100000);
 
 }
 
@@ -117,7 +137,7 @@ function refreshData(){
 }
 
 function render(resp){
-  // Structure
+  // console.log(resp)
   $('.view-container').empty()
   var htmlStruct;
   for(var i = 0; i < resp.response.length; i++){
@@ -147,11 +167,15 @@ function render(resp){
       $('.view-container').append(htmlStruct)
       renderComments(resp.response[i])
   }
-  var postStruct = `   <div class="row">
+  var postStruct = `<div class="row">
        <div class="col-xs-10 col-xs-offset-1">
-         <div class="post-button">
-           <input class="postFld"  type="text" placeholder="POST">
-            <button type="submit"  class="postBtn">Post</button>
+         <div class="post-button-container">
+          <div>
+           <input class="postFld"  type="text" placeholder="New Post"></input>
+           </div>
+           <div>
+           <button type="button" class="btn post-btn">Primary</button>
+           </div>
         </div>
        </div>
      </div>`;
@@ -170,7 +194,6 @@ for(var i = 0; i < card.comments.length; i++){
     </div>
   </div>`)
   }
-
  }
 
 

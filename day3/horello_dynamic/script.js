0bbdae83243709b03da1f38962e98d7a915cbaac
Tyrone Scafe <30609097+TScafeJR@users.noreply.github.com>
2017-09-13 15:43:28 -0700

    a new day

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,15 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+
+$('.add-list').on('click', function(event){
+  $('.add-list-form-wrapper').removeClass('collapse');
+})
+
+$('.add-list-cancel').on('click', function(event){
+  $('.add-list-form-wrapper').addClass('collapse')
+})
+
+$('.add-list-save').on('click', function(event){
+  var nice = $('input.add-list-form-wrapper').val()
+})

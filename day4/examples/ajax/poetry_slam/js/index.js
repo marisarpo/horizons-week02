3cb7fc2526f643fe207b6e89574064a48b33319c
Andros <wong22@hotmail.co.uk>
2017-09-14 10:41:57 -0700

    Finish examples

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,9 @@
 // YOUR CODE HERE
 // URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+var poem = $.ajax({
+  url: "http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+}).done(function(data){
+  var wordLength = data.split(' ').length;
+  console.log(wordLength);
+  $('#count').html(wordLength);
+});

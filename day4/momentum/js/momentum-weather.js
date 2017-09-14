361ec17459ab1cb1e989cf41a4936c6707a475b4
Andros <wong22@hotmail.co.uk>
2017-09-14 16:27:42 -0700

    Finish momentum

--- day4/momentum/js/momentum-weather.js
+++ day4/momentum/js/momentum-weather.js
@@ -5,17 +5,22 @@ window.momentum = window.momentum || {};
 // Weather
 
 momentum.WeatherCtrl = function() {
-  this.apiKey = "YOUR APP ID HERE";
+  this.apiKey = "7f654f98f64e142a745ae9e66a83cba1";
   this.apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&APPID=" + this.apiKey;
 };
 
 momentum.WeatherCtrl.prototype = {
 	// `fetchWeather(cb<Function>)` method
 	// This function should fetch the current weather in Philly by performing an AJAX call. It should pass the given cb (callback) function to the success property of the call.
-	// 
+	//
 	// hint. look into $.ajax here: http://api.jquery.com/jquery.ajax/
 	// hint. read through the documentation for the OpenWeatherAPI.
   fetchWeather: function(cb) {
 		// YOUR CODE HERE
+    $.ajax(this.apiUrl, {
+      success: function(data){
+        return cb(data);
+      }
+    })
   }
 };
--- day4/momentum/js/momentum.js
+++ day4/momentum/js/momentum.js
@@ -8,14 +8,14 @@ momentum.Core = function() {
   this.timeStr = "";
   this.quoteStr = "";
   this.weatherStr = 64;
-  
-  this.timeEl = $("YOUR SELECTOR HERE");
-  this.quoteEl = $("YOUR SELECTOR HERE");
-	this.weatherEl = $("YOUR SELECTOR HERE");
-  
+
+  this.timeEl = $("#time");
+  this.quoteEl = $("#quote");
+	this.weatherEl = $("#temperature");
+  this.location = $("#location");
   // weather controller
   this.weatherCtrl = new momentum.WeatherCtrl();
-  
+
   // quote controller
   this.quoteCtrl = new momentum.QuoteCtrl();
 };
@@ -23,30 +23,44 @@ momentum.Core = function() {
 momentum.Core.prototype = {
 	// `setTime` method
 	// This method should calculate the current time and save it to timeStr in the form HH:MM, like: 12:01 or 21:34.
-	// 
+	//
 	// hint. check out the `Date` object! Use `getHours` and `getMinutes`.
   setTime: function() {
 		// YOUR CODE HERE
+    var timeNow = new Date();
+    var hour = timeNow.getHours();
+    var minutes = timeNow.getMinutes();
+    if(minutes < 10){
+      minutes = '0' + minutes;
+    }
+    this.timeStr = hour + ':' + minutes;
   },
 	// `setQuote` method
 	// This method should set the `quoteStr` property of the momentum core. This method will be used as the callback for quoteCtrl's `fetchQuote` function.
-	// 
+	//
 	// hint. check out the `Date` object! Use `getHours` and `getMinutes`.
 	// hint. figure out what kind of response the quoteData is going to be, and see how you might be able to access the quote of the day from that.
   setQuote: function(quoteData) {
 		// YOUR CODE HERE
+    this.quoteStr = quoteData.contents.quotes[0].quote;
+    this.quoteEl.html(this.quoteStr);
   },
 	// `setWeather` method
 	// This method should set the `weatherStr` property of the momentum core. This method will be used as the callback for weatherCtrl's `fetchWeather` function.
-	// 
+	//
 	// hint. figure out what kind of response the weatherData is going to be, and see how you might be able to access the quote of the day from that.
   setWeather: function(weatherData) {
 		// YOUR CODE HERE
+    var weatherMain = weatherData.weather[0].main;
+    var mainTemp = parseInt(weatherData.main.temp -273.15);
+    this.weatherEl.html(mainTemp + '°');
+    this.location.html(weatherData.name);
   },
 	// `updateTime` method
 	// This function should call setTime() so that this.timeStr is updated.
   updateTime: function() {
 		// YOUR CODE HERE
+    setInterval(this.setTime, 30000);
   },
 	// `updateWeather` method
 	// This function should call weatherCtrl.fetchWeather and pass in this.setWeather as the callback.
@@ -54,6 +68,9 @@ momentum.Core.prototype = {
 	// note. you might run into scoping issues again. You should know how to solve them by now, using .call, .apply, or .bind.
   updateWeather: function() {
 		// YOUR CODE HERE
+    var self = this;
+
+    this.weatherCtrl.fetchWeather(this.setWeather.bind(this));
   },
 	// `updateQuote` method
 	// This function should call quoteCtrl.fetchQuote and pass in this.setQuote as the callback.
@@ -61,6 +78,7 @@ momentum.Core.prototype = {
 	// note. you might run into scoping issues again. You should know how to solve them by now, using .call, .apply, or .bind.
 	updateQuote: function() {
 		// YOUR CODE HERE
+    this.quoteCtrl.fetchQuote(this.setQuote.bind(this));
 	},
 	// `start` method
 	// This method will call some of the `update...` methods. This function will be called when the page has finished loading, so that Momentum can start off with the more up-to-date data.
@@ -72,5 +90,9 @@ momentum.Core.prototype = {
 	// ex. this.timeStr will be rendered on to the screen using this.timeEl.text(this.timeStr);
   render: function() {
 		// YOUR CODE HERE
+    this.setTime();
+    this.updateWeather();
+    this.updateQuote();
+    this.timeEl.html(this.timeStr);
   }
 };

c6674b914d570514a870b4e3db6e3ed0626f9724
Bianca-Morris <bmorris@wellesley.edu>
2017-09-13 17:51:33 -0700

    Add Parts 1 & 2 of Day 3

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,51 @@
 "use strict";
 
 // This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+
+var author;
+var comment;
+
+// Helper function that returns new comment html
+function promptUser(){
+  // prompt user for info
+  author = window.prompt("What is your name?") || "anonymous";
+  comment = window.prompt("Enter your comment here:") || "no comment";
+  // construct a new post
+  var newHtml = '<div class="comment"><div class="author">"' +
+  author + '" says:</div><div class="message">' + comment +
+  '</div> <div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>';
+  return newHtml;
+}
+
+// Post Button
+$('.post').on('click', function(){
+  var post = promptUser();
+  // appends new post to end of .comments
+  $('.comments').append(post);
+});
+
+$('.container').on('click', '.reply', function(){
+  // prompt user for info
+  var post = promptUser();
+  // appends new post to the end of this
+  var $this = $(this); // .reply
+  var commentDiv = $this.closest($('.comment'));
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.append(post);
+});
+
+$('.container').on('click', '.hide-replies', function(){
+  // find the replies div
+  var $this = $(this); // .hide-replies
+  var commentDiv = $this.closest($('.comment'));
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.hide();
+});
+
+$('.container').on('click', '.show-replies', function(){
+  // find the replies div
+  var $this = $(this); // .show-replies
+  var commentDiv = $this.closest($('.comment'));
+  var repliesDiv = commentDiv.children('.replies');
+  repliesDiv.show();
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,64 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+
+$('.add-list').on('click', function(){
+  $('.add-list-form-wrapper').removeClass('collapse');
+});
+
+$('.add-list-cancel').on('click', function(){
+  $('.add-list-form-wrapper').addClass('collapse');
+});
+
+$('.add-list-save').on('click', function(){
+  var userInput = $('#add-card-input').val();
+  var newList = '<div class="list-container"><div class="list"><div class="list-header"><span class="list-title">' + userInput
+  + '</span></div><div class="list-cards"></div><div class="list-footer"><button class="add-card">Add a card...</button><div class="collapse add-card-form-wrapper"><div class="well add-card-form"><input type="text" class="form-control" placeholder="Card title"><button type="button" class="btn btn-default add-card-save">Save</button><button type="button" class="btn btn-default add-card-cancel"><span class="glyphicon glyphicon-remove"></span></button></div> </div></div></div></div>';
+  $('.add-list-form-wrapper').addClass('collapse');
+  var listContainer = $('.add-list-form-wrapper').closest($('.list-container'));
+  listContainer.before(newList);
+});
+
+var generalize = function(d) {
+    var listFooter = d.closest($('.list-footer'));
+    var cardFormWrapper = listFooter.children('.add-card-form-wrapper');
+    return cardFormWrapper;
+}
+
+$('.board').on('click', '.add-card', function(){
+  var $this = $(this);
+  var cardFormWrapper = generalize($this);
+  cardFormWrapper.removeClass('collapse');
+});
+
+$('.board').on('click', '.add-card-cancel', function(){
+  var $this = $(this);
+  var element = generalize($this);
+  element.addClass('collapse');
+});
+
+$('.board').on('click', '.add-card-save', function(){
+  var $this = $(this);
+  var element = generalize($this);
+  var cardInput = element.find('input').val();
+  element.addClass('collapse');
+  var newCard = '<div class="card"><span class="card-more"><span class="glyphicon glyphicon-align-left"></span></span><div class="card-body">' + cardInput + '</div></div>';
+  var listFooter = element.closest('.list-footer');
+  listFooter.before(newCard);
+});
+
+var cardBeingEdited = null;
+
+$('.board').on('click', '.card', function(){
+  cardBeingEdited = $(this);
+  $('#card-edit').modal();
+  var currentName = cardBeingEdited.find('.card-body').text();
+  $('textarea').attr('placeholder', currentName);
+});
+
+$('.board').sortable({
+  // Configuration parameters here
+  connectWith: ".list-cards",
+  items: ".card",
+  revert: false
+});

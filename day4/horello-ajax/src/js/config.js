0fe7a1f50635f3129da307c485fc33f94c6a17bf
Andros <wong22@hotmail.co.uk>
2017-09-14 13:28:45 -0700

    Finish everything in horello before bonuses

--- day4/horello-ajax/src/js/config.js
+++ day4/horello-ajax/src/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "YOUR API KEY HERE";
-var apiToken = "YOUR API TOKEN HERE";
+var apiKey = "c465ffaa9afd9e2c58c651c88e54d950";
+var apiToken = "af1c68f51fcf931f3aa094b438102f8576b6972674371525d257cd5086784a1e";
 var apiUrl = "https://api.trello.com/1";
-var boardId = "YOUR BOARD ID HERE";
+var boardId = "59baceca31c5e96ee6636ca9";
--- day4/horello-ajax/src/js/script.js
+++ day4/horello-ajax/src/js/script.js
@@ -8,28 +8,116 @@ $(document).ready(function() {
 
 function createList(listName) {
   // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/lists', {
+    data: {
+      key: apiKey,
+      token: apiToken,
+      idBoard: boardId,
+      name: listName
+    },
+    method: 'POST',
+    success: function(){
+      render();
+    },
+    error: function(err){
+      console.log(err);
+    }
+  })
 }
 
 function createCard(name, listId) {
   // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/cards', {
+    data: {
+      key: apiKey,
+      token: apiToken,
+      idList: listId,
+      name: name
+    },
+    method: 'POST',
+    success: function(){
+      render();
+    },
+    error: function(err){
+      console.log(err);
+    }
+  })
 }
 
 function updateCard(title, desc, cardId) {
   // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/cards/' + cardId, {
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: title,
+      desc: desc
+    },
+    method: 'PUT',
+    success: function(){
+      render();
+    },
+    error: function(err){
+      console.log(err);
+    }
+  })
 }
 
 function render() {
   // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/boards/' + boardId, {
+  data: {
+    key: apiKey,
+    token: apiToken,
+    cards: 'all',
+    lists: 'all'
+  },
+  success: function(data) { renderBoard(data)}
+});
 }
 
 function renderBoard(board) {
   // YOUR CODE HERE
+  $('#boardAnchor').empty();
+  $('#boardAnchor').append(`<div id="$`+ boardId +`" class="board"></div>`);
+  board.lists.forEach(function(list){
+    renderList(list);
+  });
+
+  board.cards.forEach(function(card){
+    renderCard(card);
+  })
 }
 
 function renderList(list) {
   // YOUR CODE HERE
+  var listHtml = `<div class="list-container">
+  <div class="list" data-list-id="` + list.id +`" id="`+ list.id + `">
+    <div class="list-header">
+      <span class="list-title">`+ list.name + `</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card" addcardid="`+ list.id +`">Add a card...</button>
+      <div class="collapse add-card-form-wrapper" id="addCardForm`+ list.id +`">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title" id="addCardTitle`+ list.id +`" />
+          <button type="button" class="btn btn-default add-card-save" id="addCardBtn`+ list.id +`">Save</button>
+          <button type="button" class="btn btn-default add-card-cancel"><span class="glyphicon glyphicon-remove" id="addCardCancelBtn`+ list.id +`"></span></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+$('#boardAnchor .board').append(listHtml);
 }
 
 function renderCard(card) {
   // YOUR CODE HERE
+  var cardHtml = `<div id="`+ card.id +`" class="card" data-card-desc="`+ card.desc + `" data-card-name="`+ card.name + `" data-list-id="`+ card.idList + `" data-card-id="`+ card.id +`">
+  <div class="card-body">
+    `+ card.name + `
+  </div>
+</div>`
+$(' #' + card.idList).children('.list-cards').append(cardHtml);
 }

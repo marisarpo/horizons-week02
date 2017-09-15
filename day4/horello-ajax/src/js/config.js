a709e7cbc24c5a364945a3d67f83e40b39e2287a
Brandon Eng <bjeng3@gmail.com>
2017-09-14 19:04:32 -0700

    horello ajax

--- day4/horello-ajax/src/js/config.js
+++ day4/horello-ajax/src/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "YOUR API KEY HERE";
-var apiToken = "YOUR API TOKEN HERE";
+var apiKey = "8d3d8ee52fdb7a3f0291954316f4b384";
+var apiToken = "73e60e8ef4620aa03889778b76f53813280f7354cbe97456e0971bd425630fe9";
 var apiUrl = "https://api.trello.com/1";
-var boardId = "YOUR BOARD ID HERE";
+var boardId = "59bb19463c4921362e97d44e";
--- day4/horello-ajax/src/js/script.js
+++ day4/horello-ajax/src/js/script.js
@@ -8,28 +8,107 @@ $(document).ready(function() {
 
 function createList(listName) {
   // YOUR CODE HERE
+  $.ajax(apiUrl + "/lists/", {
+    method: "POST",
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: listName,
+      idBoard: boardId
+    },
+    success: function(){
+      render();
+    }
+  });
 }
 
 function createCard(name, listId) {
   // YOUR CODE HERE
+  $.ajax(apiUrl + "/cards/", {
+    method: "POST",
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: name,
+      idList: listId,
+      idBoard: boardId
+    },
+    success: function(){
+      render();
+    }
+  });
 }
 
 function updateCard(title, desc, cardId) {
   // YOUR CODE HERE
+  $.ajax(apiUrl + "/cards/" + cardId, {
+    method: "PUT",
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: title,
+      desc: desc
+    },
+    success: function(){
+      render();
+    }
+  });
 }
 
 function render() {
   // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/boards/59bb19463c4921362e97d44e', {
+    data: {
+    key: apiKey,
+    token: apiToken,
+    cards: 'all',
+    lists: 'all'
+    },
+  success: function(data) { renderBoard(data); }
+  });
 }
 
 function renderBoard(board) {
   // YOUR CODE HERE
+  $('#boardAnchor').empty();
+  $('#boardAnchor').append(`<div id="${boardId}" class="board"></div>`);
+  board.lists.forEach(function(l){
+    renderList(l);
+  });
+  board.cards.forEach(function(c){
+    renderCard(c);
+  });
 }
 
 function renderList(list) {
   // YOUR CODE HERE
+  var pasted = `<div class="list-container">
+  <div class="list" data-list-id="${list.id}" id="${list.id}">
+    <div class="list-header">
+      <span class="list-title">${list.name}</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card" addcardid="${list.id}">Add a card...</button>
+      <div class="collapse add-card-form-wrapper" id="addCardForm${list.id}">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title" id="addCardTitle${list.id}" />
+          <button type="button" class="btn btn-default add-card-save" id="addCardBtn${list.id}">Save</button>
+          <button type="button" class="btn btn-default add-card-cancel"><span class="glyphicon glyphicon-remove" id="addCardCancelBtn${list.id}"></span></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+$('#' + list.idBoard).append(pasted);
 }
 
 function renderCard(card) {
   // YOUR CODE HERE
+  var pasteagain = `<div id="${card.id}" class="card" data-card-desc="${card.desc}" data-card-name="${card.name}" data-list-id="${card.idList}" data-card-id="${card.id}">
+  <div class="card-body">
+    ${card.name}
+  </div>
+</div>`
+$('#' + card.idList).find('.list-cards').append(pasteagain);
 }

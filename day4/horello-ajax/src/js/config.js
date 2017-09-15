1e74f7d6d37c6147961809ca68b500a31e9abf52
Bianca-Morris <bmorris@wellesley.edu>
2017-09-14 20:54:28 -0700

    Add Complete Trello

--- day4/horello-ajax/src/js/config.js
+++ day4/horello-ajax/src/js/config.js
@@ -1,4 +1,4 @@
-var apiKey = "YOUR API KEY HERE";
-var apiToken = "YOUR API TOKEN HERE";
+var apiKey = "12e189f9a366fd33d56096bb774ba110";
+var apiToken = "cbd9d4f2abbd919c3493d7e148a1987fd471945f04198f6da72a8eb945897939";
 var apiUrl = "https://api.trello.com/1";
-var boardId = "YOUR BOARD ID HERE";
+var boardId = "59bb1c76518f8a24ce609d53";
--- day4/horello-ajax/src/js/script.js
+++ day4/horello-ajax/src/js/script.js
@@ -7,29 +7,108 @@ $(document).ready(function() {
 });
 
 function createList(listName) {
-  // YOUR CODE HERE
+  $.ajax({
+    url: apiUrl + `/lists?name=${listName}&idBoard=${boardId}`,
+    method: 'POST',
+    data: {
+      key: apiKey,
+      token: apiToken,
+      pos: 'bottom'
+    },
+    success: function(resp){
+      render();
+    }
+    })
 }
 
 function createCard(name, listId) {
-  // YOUR CODE HERE
+  $.ajax({
+    url: apiUrl + `/cards?idList=${listId}`,
+    method: 'POST',
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: name
+    },
+    success: function(resp){
+      render();
+    }
+  })
 }
 
 function updateCard(title, desc, cardId) {
-  // YOUR CODE HERE
+  console.log(title);
+  console.log(desc);
+  console.log(cardId);
+  $.ajax({
+    url: apiUrl + "/cards/" + cardId,
+    method: 'PUT',
+    data: {
+      key: apiKey,
+      token: apiToken,
+      name: title,
+      desc: desc
+    },
+    success: function(resp){
+      console.log('updating!')
+      render();
+    }, error: function(e){
+      console.log(e.responseText);
+    }
+  })
 }
 
 function render() {
-  // YOUR CODE HERE
+  $.ajax('https://api.Trello.com/1/boards/59bb1c76518f8a24ce609d53', {
+    data: {
+      key: "12e189f9a366fd33d56096bb774ba110",
+      token: "cbd9d4f2abbd919c3493d7e148a1987fd471945f04198f6da72a8eb945897939",
+      cards: 'all',
+      lists: 'all'
+    },
+    success: function(resp) {
+      renderBoard(resp);
+    }
+  });
 }
 
 function renderBoard(board) {
-  // YOUR CODE HERE
+  $('#boardAnchor').empty();
+  var boardID = board.id;
+  var boardHTML = '<div id="' + boardID + '" class="board"></div>';
+  $('#boardAnchor').append(boardHTML);
+  //Iterate through board.lists and pass each list object through the renderList function.
+  board.lists.forEach(function(list){
+    renderList(list);
+  });
+  board.cards.forEach(function(card){
+    renderCard(card);
+  });
 }
 
 function renderList(list) {
-  // YOUR CODE HERE
+  var boardID = list.idBoard;
+  console.log(boardID);
+  var listID = list.id;
+  var listName = list.name;
+  var listWrapperHTML = '<div class="list-container"><div class="list" data-list-id="'+
+    listID + '"id="' + listID
+    + '"><div class="list-header"><span class="list-title">' + listName + '</span></div><div class="list-cards"></div><div class="list-footer"><button class="add-card" addcardid="'
+    + listID + '">Add a card...</button><div class="collapse add-card-form-wrapper" id="addCardForm'
+    + listID + '"><div class="well add-card-form"><input type="text" class="form-control" placeholder="Card title" id="addCardTitle'
+    + listID + '" /><button type="button" class="btn btn-default add-card-save" id="addCardBtn'
+    + listID + '">Save</button><button type="button" class="btn btn-default add-card-cancel"><span class="glyphicon glyphicon-remove" id="addCardCancelBtn'
+    + listID + '"></span></button></div></div></div></div></div>';
+  $('#' + boardID).append(listWrapperHTML);
 }
 
 function renderCard(card) {
-  // YOUR CODE HERE
+  var cardID = card.id;
+  var cardDescription = card.desc;
+  var cardName = card.name;
+  var listID = card.idList;
+  var cardHTML = `<div id="` + cardID + `" class="card" data-card-desc="` +
+    cardDescription + `" data-card-name="` + cardName + `" data-list-id="` + listID
+    + `" data-card-id="` + cardID + `"><div class="card-body">` + cardName + `</div></div>`
+  $('#'+ listID + ' .list-cards').append(cardHTML);
 }

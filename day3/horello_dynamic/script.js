471561afa9e9a80a80322543dbff5626ef032627
katslump <kat.slump@gmail.com>
2018-01-24 15:44:01 -0800

    Up to part 3 in horello trello

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var cardBeingEdited = null;
+
 $(document).ready(function (){
     $('.add-list').on('click', function() {
         $('.add-list-form-wrapper').toggleClass('collapse');
@@ -23,7 +25,7 @@ $(document).ready(function (){
               <button class="add-card">Add a card...</button>
               <div class="collapse add-card-form-wrapper">
                 <div class="well add-card-form">
-                  <input type="text" class="form-control" placeholder="Card title">
+                  <input type="text" class="form-control" placeholder="Card title" id="add-card">
                   <button type="button" class="btn btn-default add-card-save">
                     Save
                   </button>
@@ -38,6 +40,50 @@ $(document).ready(function (){
         $('.add-list-form-wrapper').before(newContent);
         $('#add-list').val('');
     });
+
+    $('.list').on('click', '.add-card', function() {
+        $(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+        $('#add-card').val('');
+    });
+
+    $('.list').on('click', '.add-card-cancel', function() {
+        $('.add-card-form-wrapper').addClass('collapse');
+        $('#add-card').val('');
+    });
+
+    $('.list').on('click', '.add-card-save', function() {
+        var cardTitle = $(this).siblings().eq(0).val();
+        var newContent = $(`<div class="card">
+          <span class="card-more">
+            <span class="glyphicon glyphicon-align-left"></span>
+          </span>
+          <div class="card-body">` + cardTitle + `</div>
+        </div>`);
+        $(this).parent().parent().parent().before(newContent);
+        $(this).siblings().eq(0).val('');
+        $('.add-card-form-wrapper').addClass('collapse');
+    });
+
+    $('.board').on('click', '.card', function() {
+        cardBeingEdited = $(this);
+        $(document).find("#card-edit").modal("show");
+        $(document).find("#card-edit-body").val($(this).text().trim());
+    });
+
+    $(".card-edit-save").on('click', function() {
+        var newVal = $(document).find("#card-edit-body").val();
+        cardBeingEdited.find("div.card-body").text(newVal);
+    });
+
+    $(".card-edit-cancel").on('click', function() {
+        $(document).find("#card-edit").modal("hide");
+    });
+
+
+
+
+
+
 
 
 

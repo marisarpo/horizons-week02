f3f2d4feeb8f538eff53b8978195c71bf91b2e41
James Wang <j.wang5441@gmail.com>
2018-01-24 13:52:18 -0800

    Half way through comments

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,39 @@
 "use strict";
 
+$(document).ready(function() {
+  $('.post').on('click', function(){
+    var author = prompt('Enter Author Name')
+    var comment = prompt('Enter comment')
+    var newContent = $(`<div class="comment">
+        <div class="author">` + author +` says:</div>
+        <div class="message">`+ comment + `</div>
+        <div class="controls">
+          <button class="hide-replies btn btn-default">Hide Replies</button>
+          <button class="show-replies btn btn-default">Show Replies</button>
+          <button class="reply btn btn-default">Reply</button>
+        </div>
+        <div class="replies"></div>
+      </div>`);
+    $('.comments').append(newContent);
+  });
+  $('.comments').on('click','.reply', function() {
+    var author = prompt('Enter Author Name')
+    var comment = prompt('Enter comment')
+    var newContent = $(`<div class="comment">
+        <div class="author">` + author +` says:</div>
+        <div class="message">`+ comment + `</div>
+        <div class="controls">
+          <button class="hide-replies btn btn-default">Hide Replies</button>
+          <button class="show-replies btn btn-default">Show Replies</button>
+          <button class="reply btn btn-default">Reply</button>
+        </div>
+        <div class="replies"></div>
+      </div>`);
+    var $this = $(this);
+    var commentDiv = $this.closest('.comment');
+    var repliesDiv = commentDiv.children('.replies');
+    repliesDiv.append(newContent);
+  });
+})
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE

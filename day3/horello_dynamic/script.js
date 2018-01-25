6fe902d38e90f152d26e4ee4649b62e9515b7677
Persis Ratouis <persis.ratouis@gmail.com>
2018-01-24 18:50:52 -0800

    working through part 3

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,83 @@
 "use strict";
-
+var cardBeingEdited = null;
 // YOUR JAVASCRIPT CODE GOES HERE
+$(document).ready(function() {
+
+  $('.add-list').click(function(event) {
+    $(this).siblings('.add-list-form-wrapper').removeClass('collapse');
+  })
+
+  $('.add-list-cancel').click(function(event) {
+    $(this).closest('.add-list-form-wrapper').addClass('collapse');
+  })
+
+  $('.add-list-save').click(function(event) {
+    var newList = `<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title">${$(this).siblings('input').val()}</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+            Save
+          </button>
+          <button type="button" class="btn btn-default add-card-cancel">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+      </div>
+    </div></div></div>`
+    $(this).closest('.list-container').before(newList);
+    $(this).closest('.add-list-form-wrapper').addClass('collapse');
+
+  })
+
+  $('.board').on('click', '.add-card', function(event) {
+    $(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+  })
+
+  $('.board').on('click', '.add-card-cancel', function(event) {
+    $(this).closest('.add-card-form-wrapper').addClass('collapse');
+  })
+
+  $('.board').on('click', '.add-card-save', function(event) {
+    var newCard = `<div class="card">
+      <span class="card-more">
+        <span class="glyphicon glyphicon-align-left"></span>
+        </span>
+        <div class="card-body">${$(this).siblings('input').val()}</div>
+        </div>`
+
+    $(this).closest('.list').children('.list-cards').append(newCard);
+    $(this).siblings('input').val('');
+    $(this).closest('.add-card-form-wrapper').addClass('collapse');
+  })
+
+  /*$('.board').on('click','.card', function(event){
+    cardBeingEdited = this;
+    var modal = $(cardBeingEdited).parents().find('#card-edit');
+    var cardBody = $(cardBeingEdited).find('.card-body');
+    modal.find('#card-edit-body').val(cardBody.text());
+    modal.modal();
+
+  })*/
+
+  $('.card-edit-save').on('click', function(event){
+    var modal = $(this).parents().find('#card-edit');
+    var cardBody = $(cardBeingEdited).find('.card-body');
+    cardBody.text(modal.find('#card-edit-body').val());
+    modal.modal('hide');
+  })
+
+
+  $('.list-cards').sortable({
+    connectWith: '.list-cards'
+  })
+
+
+})

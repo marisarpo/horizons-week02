04a6675f5b0f7d10902c4eac2ef62de57b113b8a
katslump <kat.slump@gmail.com>
2018-01-25 09:31:53 -0800

    Population example done

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -2,20 +2,20 @@
 
 var cardBeingEdited = null;
 
-$(document).ready(function (){
-    $('.add-list').on('click', function() {
-        $('.add-list-form-wrapper').toggleClass('collapse');
-        $('#add-list').val('');
-    });
+$(document).ready(function() {
+  $('.add-list').on('click', function() {
+    $('.add-list-form-wrapper').toggleClass('collapse');
+    $('#add-list').val('');
+  });
 
-    $('.add-list-cancel').on('click', function() {
-        $('.add-list-form-wrapper').addClass('collapse');
-        $('#add-list').val('');
-    });
+  $('.add-list-cancel').on('click', function() {
+    $('.add-list-form-wrapper').addClass('collapse');
+    $('#add-list').val('');
+  });
 
-    $('.add-list-save').on('click', function() {
-        var listTitle = $('#add-list').val();
-        var newContent = $(`
+  $('.add-list-save').on('click', function() {
+    var listTitle = $('#add-list').val();
+    var newContent = $(`
         <div class="list-container">
           <div class="list">
             <div class="list-header">
@@ -38,55 +38,54 @@ $(document).ready(function (){
             </div>
           </div>
         </div>`);
-        $(this).parent().parent().parent().parent().before(newContent);
-        $('.add-list-form-wrapper').toggleClass('collapse');
-        $('#add-list').val('');
-    });
+    $(this).parent().parent().parent().parent().before(newContent);
+    $('.add-list-form-wrapper').toggleClass('collapse');
+    $('#add-list').val('');
+  });
 
-    $('.board').on('click', '.add-card', function() {
-        $(this).siblings('.add-card-form-wrapper').toggleClass('collapse');
-        $('#add-card').val('');
-    });
+  $('.board').on('click', '.add-card', function() {
+    $(this).siblings('.add-card-form-wrapper').toggleClass('collapse');
+    $('#add-card').val('');
+  });
 
-    $('.board').on('click', '.add-card-cancel', function() {
-        $('.add-card-form-wrapper').addClass('collapse');
-        $('#add-card').val('');
-    });
+  $('.board').on('click', '.add-card-cancel', function() {
+    $('.add-card-form-wrapper').addClass('collapse');
+    $('#add-card').val('');
+  });
 
-    $('.board').on('click', '.add-card-save', function() {
-        var cardTitle = $(this).siblings().eq(0).val();
-        var newContent = $(`<div class="card">
+  $('.board').on('click', '.add-card-save', function() {
+    var cardTitle = $(this).siblings().eq(0).val();
+    var newContent = $(`<div class="card">
           <span class="card-more">
             <span class="glyphicon glyphicon-align-left"></span>
           </span>
           <div class="card-body">` + cardTitle + `</div>
         </div>`);
-        $(this).parent().parent().parent().before(newContent);
-        $(this).siblings().eq(0).val('');
-        $('.add-card-form-wrapper').addClass('collapse');
-    });
+    $(this).parent().parent().parent().parent().(newContent);
+    $(this).siblings().eq(0).val('');
+    $('.add-card-form-wrapper').addClass('collapse');
+  });
 
-    $('.board').on('click', '.card', function() {
-        cardBeingEdited = $(this);
-        $(document).find("#card-edit").modal("show");
-        $(document).find("#card-edit-body").val($(this).text().trim());
-    });
+  $('.board').on('click', '.card', function() {
+    cardBeingEdited = $(this);
+    $(document).find("#card-edit").modal("show");
+    $(document).find("#card-edit-body").val($(this).text().trim());
+  });
 
-    $(".card-edit-save").on('click', function() {
-        var newVal = $(document).find("#card-edit-body").val();
-        cardBeingEdited.find("div.card-body").text(newVal);
-    });
+  $(".card-edit-save").on('click', function() {
+    var newVal = $(document).find("#card-edit-body").val();
+    cardBeingEdited.find("div.card-body").text(newVal);
+  });
 
-    $(".card-edit-cancel").on('click', function() {
-        $(document).find("#card-edit").modal("hide");
-    });
+  $(".card-edit-cancel").on('click', function() {
+    $(document).find("#card-edit").modal("hide");
+  });
 
-    $('.list-cards').sortable({
+  $('.list-cards').sortable({
 
-    });
+  });
 
-    $('.board').sortable({
-    });
+  $('.board').sortable({});
 
 
 

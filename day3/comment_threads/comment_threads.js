a1f48f119268ab574ddadeb6c5cad6af575c3b8b
smedlang <smedlang@gmail.com>
2018-06-06 14:05:41 -0700

    week 2 day 3

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -1,4 +1,27 @@
 "use strict";
 
-// This file contains JavaScript that will run on your page.
-// YOUR CODE HERE
+$('.post').on('click', function(){
+  var author = window.prompt('Enter your name');
+  var comment = window.prompt('Enter your comment');
+  $('.comments').append('<div class = "comment"><div class="author">"' + author + '" says:</div><div class="message">'+ comment + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>');
+});
+
+$(document).on('click', '.reply', function(){
+  var author = window.prompt('Enter your name');
+  var comment = window.prompt('Enter your comment');
+  var divToAppend=$(this).closest('.comment').children('.replies');
+  divToAppend.append('<div class = "comment"><div class="author">"' + author + '" says:</div><div class="message">'+ comment + '</div><div class="controls"><button class="hide-replies btn btn-default">Hide Replies</button><button class="show-replies btn btn-default">Show Replies</button><button class="reply btn btn-default">Reply</button></div><div class="replies"></div></div>');
+});
+
+$(document).on('click', '.hide-replies', function(){
+  $(this).closest('.comment').children('.replies').hide();
+  $(this).closest('.comment').children('.controls').children('.hide-replies').hide();
+  $(this).closest('.comment').children('.controls').children('.show-replies').show();
+
+})
+
+$(document).on('click', '.show-replies', function(){
+  $(this).closest('.comment').children('.replies').show();
+  $(this).closest('.comment').children('.controls').children('.show-replies').hide();
+  $(this).closest('.comment').children('.controls').children('.hide-replies').show();
+})

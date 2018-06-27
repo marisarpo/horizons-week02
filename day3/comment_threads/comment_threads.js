cb1721a7ae21e9df74cf3c0c2099bd6e4bf8547e
Rohan <rohankanchana@github.com>
2018-06-27 16:37:05 -0700

    day 3 changes

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,74 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$(document).ready(function() {
+	$('.show-replies').hide();
+});
+
+$("button.post").on('click', function() {
+	var author = prompt("Author?");
+	var comment = prompt("Comment?");
+
+	var newDiv = `<div class="comment">
+          <div class="author">${author} says:</div>
+          <div class="message">${comment}</div>
+          <div class="controls">
+            <button class="hide-replies btn btn-default">Hide Replies</button>
+            <button class="show-replies btn btn-default">Show Replies</button>
+            <button class="reply btn btn-default">Reply</button>
+          </div>
+          <div class="replies"></div>
+        </div>`;
+    $('.comments').append(newDiv);
+});
+
+$('.comments').on('click', '.reply', function(){
+	var author = prompt("Author?");
+	var comment = prompt("Comment?");
+
+	var newDiv = `<div class="comment">
+          <div class="author">${author} says:</div>
+          <div class="message">${comment}</div>
+          <div class="controls">
+            <button class="hide-replies btn btn-default">Hide Replies</button>
+            <button class="show-replies btn btn-default">Show Replies</button>
+            <button class="reply btn btn-default">Reply</button>
+          </div>
+          <div class="replies"></div>
+        </div>`;
+
+    $(this).closest('.comment').children('.replies').append(newDiv);
+});
+
+function getParagraph(count) {
+	return "<p class=\"deleter\">" + String(count) + " Replies</p>";
+}
+
+function getCount(jelm) {
+  var len = jelm.children('.replies').children().length;
+  if (len === 0 || len === undefined) {
+    return 0;
+  }
+
+  var total = jelm.children('.replies').children().length;
+
+  jelm.children('.replies').children().each(function() {
+    total += getCount($(this));
+  });
+
+  return total;
+}
+
+$('.comments').on('click', '.hide-replies', function() {
+	$(this).closest('.comment').children('.replies').hide();
+	$(this).closest('.comment').find('.hide-replies').hide();
+	$(this).closest('.comment').find('.show-replies').show();
+	$(this).closest('.comment').find('.controls').append(getParagraph(getCount($(this).parent().parent())));
+});
+
+$('.comments').on('click', '.show-replies', function() {
+	$(this).closest('.comment').children('.replies').show();
+	$(this).closest('.comment').find('.show-replies').hide();
+	$(this).closest('.comment').find('.hide-replies').show();
+	$(this).closest('.comment').find('.deleter').remove();
+});
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,148 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+var cardBeingEdited = null;
+var colorArray = [
+  "red", "orange", "yellow", "green", "blue", "purple", "white", 
+  "brown"
+];
+var curTopbarIndex = 0;
+var curBackgroundIndex = 1;
+var curListIndex = 2;
+var curCardIndex = 3;
+
+function incrementIndexes() {
+  curTopbarIndex += 1;
+  if (curTopbarIndex >= colorArray.length) {
+    curTopbarIndex -= colorArray.length;
+  }
+
+  curBackgroundIndex += 1;
+  if (curBackgroundIndex >= colorArray.length) {
+    curBackgroundIndex -= colorArray.length;
+  }
+
+  curListIndex += 1;
+  if (curListIndex >= colorArray.length) {
+    curListIndex -= colorArray.length;
+  }
+
+  curCardIndex += 1;
+  if (curCardIndex >= colorArray.length) {
+    curCardIndex -= colorArray.length;
+  }
+  console.log(curCardIndex);
+}
+
+$(document).on('keydown', function(event) {
+  if (event.key === "c") {
+    $('.card:hover').remove();
+  }
+});
+
+function setTheme() {
+  $('header').css('background-color', colorArray[curTopbarIndex]);
+  $('body').css('background-color', colorArray[curBackgroundIndex]);
+  $('.list').css('background-color', colorArray[curListIndex]);
+  $('.card').css('background-color', colorArray[curCardIndex]);
+  $('.board').css('background', 'url("https://cdn-images-1.medium.com/max/1600/1*EuHesBptq_s-R37YJdIbaQ.png") no-repeat center');
+}
+
+$('select').on('change', function() {
+  var value = Number($(this).val());
+
+  if (value === 0) {
+    curTopbarIndex = colorArray.length-1;
+  } else {
+    curTopbarIndex = value;
+  }
+  curBackgroundIndex = value;
+  curListIndex = value+1;
+  curCardIndex = value+2;
+  incrementIndexes();
+  setTheme();
+});
+
+$('.header-logo').on('click', function() {
+  setTheme();
+  incrementIndexes();
+});
+
+
+$('.list-cards').sortable({
+      connectWith: ".list-cards"
+}).disableSelection();
+
+$('.board').on('click', '.card', function() {
+	cardBeingEdited = $(this);
+
+	$('#card-edit').modal();
+	$('#card-edit-body').val($(this).find('.card-body').text());
+});
+
+$('.card-edit-save').on('click', function() {
+	cardBeingEdited.find('.card-body').html($('#card-edit-body').val());
+
+	$('#card-edit').modal('toggle');
+});
+
+$('.add-card').on('click', function() {
+	$(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+});
+
+$('.add-card-cancel').on('click', function() {
+	$(this).parent().parent().addClass('collapse');
+});
+
+$('.add-card-save').on('click', function() {
+	var cardTitle = $(this).parent().children('.form-control').val();
+
+	var card = `<div class="card">
+          <span class="card-more">
+            <span class="glyphicon glyphicon-align-left"></span>
+          </span>
+          <div class="card-body">${cardTitle}</div>
+        </div>`;
+    $(this).parent().parent().parent().siblings('.list-cards').append(card);
+    $(this).parent().parent().hide();
+});
+
+$('.add-list').on('click', function() {
+	$('.add-list-form-wrapper').removeClass('collapse');
+});
+
+$('.add-list-cancel').on('click', function() {
+	$('.add-list-form-wrapper').addClass('collapse');
+});
+
+$('.add-list-save').on('click', function() {
+	var title = $(this).parent('.add-card-form').find('.form-control').val();
+
+	var list = `<div class="list-container">
+          <div class="list">
+            <div class="list-header">
+              <span class="list-title">${title}</span>
+            </div>
+            <div class="list-cards"></div>
+            <div class="list-footer">
+              <button class="add-card">Add a card...</button>
+              <div class="collapse add-card-form-wrapper">
+                <div class="well add-card-form">
+                  <input type="text" class="form-control" placeholder="Card title">
+                  <button type="button" class="btn btn-default add-card-save">
+                    Save
+                  </button>
+                  <button type="button" class="btn btn-default add-card-cancel">
+                    <span class="glyphicon glyphicon-remove"></span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+
+    $('.board').prepend(list);
+
+    $(this).parent('.add-card-form').find('.form-control').val("");
+    $('.add-list-form-wrapper').addClass('collapse');
+});

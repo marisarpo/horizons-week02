3f3b9ef1575fc785000a32cfd5c48b66f1d2173a
Joseph <josephocharo7@gmail.com>
2018-06-27 17:37:34 -0400

    day 3, comment threads, part 2 complete

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -15,9 +15,38 @@ $(".post").on('click', function(event){
           <button class="show-replies btn btn-default">Show Replies</button>
           <button class="reply btn btn-default">Reply</button>
         </div>
+
       </div>`);
   console.log(name);
   console.log(comment);
-
-
 })
+
+$(".comments").on('click','button.reply' ,function() {
+  // `this` points to the current `.reply` button that was clicked
+  var $this = $(this);
+  var commentDiv = $this.closest('.comment');
+  var repliesDiv = commentDiv.children('div.replies');
+
+  var name = prompt("Enter your name");
+  var reply = prompt("Enter your reply");
+
+  var comment = `<div class="comment">
+      <div class="author">${name} says:</div>
+      <div class="message">${reply}</div>
+      <div class="controls">
+        <button class="hide-replies btn btn-default">Hide Replies</button>
+        <button class="show-replies btn btn-default">Show Replies</button>
+        <button class="reply btn btn-default">Reply</button>
+      </div>
+      <div class="replies"></div>
+    </div>`;
+
+  if (repliesDiv.get(0) === undefined){
+    repliesDiv = $(`<div class="replies"></div>`);
+    repliesDiv.append(comment);
+    commentDiv.append(repliesDiv);
+  }else{
+    repliesDiv.append(comment);
+  }
+
+});

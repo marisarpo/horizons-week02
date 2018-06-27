e10d03b9667270bf266abe953626fe38615d5f7f
Joseph <josephocharo7@gmail.com>
2018-06-27 18:54:49 -0400

    day 3, horello dynamic, part 1 complete

--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,65 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+$('.add-list').on('click', function(event){
+    $('.add-list-form-wrapper').removeClass('collapse');
+});
+
+$('.add-list-cancel').on('click', function(event){
+    $('.add-list-form-wrapper').addClass('collapse');
+});
+
+$('button.add-list-save').on('click', function(event){
+    var title = $('.add-list-form-wrapper input').val();
+    var list = `
+    <div class="list-container">
+    <div class="list">
+      <div class="list-header">
+        <span class="list-title">${title}</span>
+      </div>
+      <div class="list-cards"></div>
+      <div class="list-footer">
+        <button class="add-card">Add a card...</button>
+        <div class="collapse add-card-form-wrapper">
+          <div class="well add-card-form">
+            <input type="text" class="form-control" placeholder="Card title">
+            <button type="button" class="btn btn-default add-card-save">
+              Save
+            </button>
+            <button type="button" class="btn btn-default add-card-cancel">
+              <span class="glyphicon glyphicon-remove"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`;
+    $(this).closest('.list-container')
+    .before(`
+    <div class="list-container">
+    <div class="list">
+      <div class="list-header">
+        <span class="list-title">${title}</span>
+      </div>
+      <div class="list-cards"></div>
+      <div class="list-footer">
+        <button class="add-card">Add a card...</button>
+        <div class="collapse add-card-form-wrapper">
+          <div class="well add-card-form">
+            <input type="text" class="form-control" placeholder="Card title">
+            <button type="button" class="btn btn-default add-card-save">
+              Save
+            </button>
+            <button type="button" class="btn btn-default add-card-cancel">
+              <span class="glyphicon glyphicon-remove"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`);
+    $('.add-list-form-wrapper').addClass('collapse');
+    $('.add-list-form-wrapper').val('');
+
+
+});

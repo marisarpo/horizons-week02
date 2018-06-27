75363f30fd39b215566c96239c71c9a8416c8cfb
Joseph <josephocharo7@gmail.com>
2018-06-27 16:52:47 -0400

    day 3, comment threads, part 1 complete

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,22 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+$(".post").on('click', function(event){
+  var name = prompt("Enter your name");
+  var comment = prompt("Enter your comment");
+  $(".comments>:last-child")
+  .after(
+    `<div class="comment">
+        <div class="author">${name} says:</div>
+        <div class="message">${comment}</div>
+        <div class="controls">
+          <button class="hide-replies btn btn-default">Hide Replies</button>
+          <button class="show-replies btn btn-default">Show Replies</button>
+          <button class="reply btn btn-default">Reply</button>
+        </div>
+      </div>`);
+  console.log(name);
+  console.log(comment);
+
+
+})

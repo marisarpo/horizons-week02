7cc2c19b7de2def012d0bf18001e2069e317945b
fkhan <fkhan@bates.edu>
2018-06-27 19:30:02 -0700

    Day3 7:29PM

--- day3/comment_threads/comment_threads.js
+++ day3/comment_threads/comment_threads.js
@@ -2,3 +2,48 @@
 
 // This file contains JavaScript that will run on your page.
 // YOUR CODE HERE
+
+$('button.post').on('click', function(event) {
+  event.preventDefault();
+  var author = prompt("Input author name");
+  var comment = prompt("Input comment");
+  var myText = `<div class="comment">
+  <div class="author"> ${author} says:</div>
+  <div class="message"> ${comment} </div>
+  <div class="controls">
+    <button class="hide-replies btn btn-default">Hide Replies</button>
+    <button class="show-replies btn btn-default">Show Replies</button>
+    <button class="reply btn btn-default">Reply</button>
+  </div>
+  <div class="replies"></div>
+</div>`;
+  $('.comments').append(myText);
+}
+)
+
+$('.comments').on('click', '.reply', function(event){
+  event.preventDefault();
+  var author = prompt("Input author name");
+  var comment = prompt("Input comment");
+  var myText = `<div class="comment">
+  <div class="author"> ${author} says:</div>
+  <div class="message"> ${comment} </div>
+  <div class="controls">
+    <button class="hide-replies btn btn-default">Hide Replies</button>
+    <button class="show-replies btn btn-default">Show Replies</button>
+    <button class="reply btn btn-default">Reply</button>
+  </div>
+  <div class="replies"></div>
+</div>`;
+  $(this).closest('.comment').children('.replies').append(myText);
+})
+
+$('.comments').on('click', '.hide-replies', function(event){
+  event.preventDefault();
+  $(this).closest('.comment').children('.replies').hide();
+})
+
+$('.comments').on('click', '.show-replies', function(event){
+  event.preventDefault();
+  $(this).closest('.comment').children('.replies').show();
+})
--- day3/horello_dynamic/script.js
+++ day3/horello_dynamic/script.js
@@ -1,3 +1,117 @@
 "use strict";
 
 // YOUR JAVASCRIPT CODE GOES HERE
+var cardBeingEdited = null;
+
+var listIds = ['#sortable1','#sortable2','#sortable3'];
+
+$('.add-list').on('click', function (event) {
+  event.preventDefault();
+  $('.add-list-form-wrapper').removeClass('collapse')
+})
+
+
+$('.add-list-cancel').on('click', function (event) {
+  event.preventDefault();
+  $('.add-list-form-wrapper').addClass('collapse')
+})
+
+
+$('.add-list-save').on('click', function (event) {
+  event.preventDefault();
+  var title = $(this).closest('.add-card-form').children('input').val();
+  var myHTML = `<div class="list-container">
+  <div class="list">
+    <div class="list-header">
+      <span class="list-title">${title}</span>
+    </div>
+    <div class="list-cards"></div>
+    <div class="list-footer">
+      <button class="add-card">Add a card...</button>
+      <div class="collapse add-card-form-wrapper">
+        <div class="well add-card-form">
+          <input type="text" class="form-control" placeholder="Card title">
+          <button type="button" class="btn btn-default add-card-save">
+            Save
+          </button>
+          <button type="button" class="btn btn-default add-card-cancel">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+  $(this).closest('.list-container').before(myHTML);
+  $(this).closest('.add-card-form').children('input').val('');
+  $('.add-list-form-wrapper').addClass('collapse');
+  $('.list-cards').sortable({
+    revert: false,
+    items: '.card',
+    connectWith: '.list-cards'
+  });
+
+})
+
+
+$('.board').on('click', '.add-card', function(event) {
+  event.preventDefault();
+  $(this).siblings('.add-card-form-wrapper').removeClass('collapse');
+})
+
+$('.board').on('click', '.add-card-cancel', function(event) {
+  event.preventDefault();
+  $(this).closest('.add-card-form-wrapper').addClass('collapse');
+})
+
+$('.board').on('click', '.add-card-save', function (event) {
+  event.preventDefault();
+  var title = $(this).siblings('input').val();
+  var myHTML = `<div class="card">
+  <span class="card-more">
+    <span class="glyphicon glyphicon-align-left"></span>
+  </span>
+  <div class="card-body">${title}</div>
+</div>`;
+  $(this).closest('.list-footer').siblings('.list-cards').append(myHTML);
+  $(this).siblings('input').val('');
+  $(this).closest('.add-card-form-wrapper').addClass('collapse');
+})
+
+$('.board').on('click', '.card', function (event) {
+  event.preventDefault();
+  cardBeingEdited = $(this);
+  $('#card-edit').modal();
+  var oldVal = cardBeingEdited.children('.card-body').text();
+  $('textarea#card-edit-body').val(oldVal);
+})
+
+$('.card-edit-save').on('click', function (event) {
+  event.preventDefault();
+  var newVal = $('textarea#card-edit-body').val();
+  cardBeingEdited.children('.card-body').text(newVal);
+  $('#card-edit').modal('toggle');
+})
+
+$('.list-cards').sortable({
+  revert: true,
+  items: '.card',
+  connectWith: '.list-cards'
+});
+
+//
+// $( function() {
+//     $( "#sortable1, #sortable2, #sortable3" ).sortable({
+//       connectWith: ".connectedSortable"
+//     }).disableSelection();
+//   } );
+
+// function() {
+//   var mapped = listIds.map( (x) => {return '#'+x} );
+//   var string = mapped.join(', ');
+//   $( function() {
+//       $( string ).sortable({
+//         connectWith: ".connectedSortable"
+//       }).disableSelection();
+//     });
+// }

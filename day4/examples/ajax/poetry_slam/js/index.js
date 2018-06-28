8c8b1995855fbab0b0c322aea5bffcb204380e3f
Abraham Hamidi <abraham.hamidi01@gmail.com>
2018-06-28 13:31:16 -0700

    Day 4 morning exercises

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,4 @@
-// YOUR CODE HERE
-// See poetry_slam/index.html for instructions
+$.ajax("https://horizons-school-of-technology.github.io/week02/day4/examples/poem.txt")
+        .done(function(data) {
+    $("#count").text(data.split(" ").length);
+});

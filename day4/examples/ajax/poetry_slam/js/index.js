f35f34f958c42a36e57f1c1de03abb3a86b90a66
Kitan Garcia <kitangar@gmail.com>
2018-06-28 14:12:44 -0700

    fix errors

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,16 @@
 // YOUR CODE HERE
 // See poetry_slam/index.html for instructions
+$.ajax({
+  url: "https://horizons-school-of-technology.github.io/week02/day4/examples/poem.txt",
+  success: function (response)
+  {
+    $("body").append($("<pre>").text(response));
+    var poemArr = response.split(" ");
+    console.log(poemArr);
+    var wordCount = poemArr.length;
+    console.log(wordCount);
+    $("#count").text(wordCount);
+  }
+})
+
+

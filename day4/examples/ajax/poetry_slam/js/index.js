18b3e7dda51fcf9682e5120c0924dc02e71cd5e5
Monkey <ole.orke@hotmail.com>
2018-06-28 11:46:03 -0700

    finish morning excercises

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE
 // See poetry_slam/index.html for instructions
+
+$.ajax({
+  url: "https://horizons-school-of-technology.github.io/week02/day4/examples/poem.txt",
+  success: function(response) {
+    var respStr = response;
+    var respArr = respStr.split(" ");
+    $("#count").text(respArr.length);
+  }
+});

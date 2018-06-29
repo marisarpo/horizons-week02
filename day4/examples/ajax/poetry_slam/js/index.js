dc7a03a38c51506d28a3873984d3c1d92aa10ef3
Joseph <josephocharo7@gmail.com>
2018-06-28 20:21:04 -0400

    ajax examples complete

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,9 @@
 // YOUR CODE HERE
 // See poetry_slam/index.html for instructions
+$.ajax({
+  url: 'https://horizons-school-of-technology.github.io/week02/day4/examples/poem.txt',
+  success: function(response){
+
+    $('#count').text(response.split(' ').length);
+  }
+})

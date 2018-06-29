cc181697dc591a4445c447d6e1ed82debbdd7cb9
khalidwilliams <khalidwill27@gmail.com>
2018-06-28 18:21:59 -0700

    first attempt at asynch

--- day4/examples/ajax/poetry_slam/js/index.js
+++ day4/examples/ajax/poetry_slam/js/index.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE
 // See poetry_slam/index.html for instructions
+
+$.ajax({
+  url: "http://horizons-json-cors.s3.amazonaws.com/poem.txt",
+  success: function(resp) {
+    var wordCount = null;
+    wordCount = resp.split(" ").length;
+    $('#count').text(wordCount);
+  }
+})
